84b1271c176e0ce3af1c2ff31ebd887cfd8f7c8e
barnabytprowe <barnaby.t.p.rowe@gmail.com>
2013-01-25 10:48:32 +0000

    modified plotting script to include new QZ2 (with abs) (#12)

--- plot_const_shear_metrics.py
+++ plot_const_shear_metrics.py
@@ -6,8 +6,8 @@ NIMS = 300               # Number of images per set, always 200 for G10
 NGALS_PER_IM = 10000     # In GREAT08/GREAT10 there were 10000 galaxies per image
 TRUE_SIGMA = 0.04        # Standard deviation of true input shears for normal distribution
 TRUE_RANGE = 0.08        # Range of true input shears for a uniform distribution
-NTRUESETS = 50           # Don't necessarily need to have NIMS input shears. But easiest if NTRUE an
-                         # integral fraction of NIMS..
+NTRUESETS = 50           # Don't necessarily need to have NIMS input shears. But easiest if
+                         # NTRUESETS is an integral fraction of NIMS..
 
 CFID = 2.e-4 # Fiducial, "target" m and c values
 MFID = 2.e-3 #
@@ -46,21 +46,23 @@ for j in range(NMONTE):
     # Loop over mvalues making indepented submissions at each c, m combination
     for i in range(NBINS):
         g1sub, g2sub = g3utils.make_submission_const_shear(CFID, CFID, mvals[i], mvals[i],
-                                                           g1true, g2true, ngals_per_im=NGALS_PER_IM,
+                                                           g1true, g2true,
+                                                           ngals_per_im=NGALS_PER_IM,
                                                            noise_sigma=NOISE_SIGMA)
-        Q08_vs_m[i] = g3utils.metricQ08_const_shear(g1sub, g2sub, g1true, g2true, ntruesets=NTRUESETS)
+        Q08_vs_m[i] = g3utils.metricQ08_const_shear(g1sub, g2sub, g1true, g2true,
+                                                    ntruesets=NTRUESETS)
         QZ1_vs_m[i] = g3utils.metricQZ1_const_shear(g1sub, g2sub, g1true, g2true,
                                                     cfid=CFID, mfid=MFID)[0]
-        #QZ2_vs_m[i] = g3utils.metricQZ2_const_shear(g1sub, g2sub, g1true, g2true,
-        #                                            cfid=CFID, mfid=MFID)[0]
+        QZ2_vs_m[i] = g3utils.metricQZ2_const_shear(g1sub, g2sub, g1true, g2true,
+                                                    cfid=CFID, mfid=MFID)[0]
 
     if j == 0:
         plt.loglog(mvals, QZ1_vs_m, 'r', label='QZ1')
-        #plt.loglog(mvals, QZ2_vs_m, 'g', label='QZ2')
+        plt.loglog(mvals, QZ2_vs_m, 'g', label='QZ2')
         plt.loglog(mvals, Q08_vs_m, 'b', label='Q08')
     else:
         plt.loglog(mvals, QZ1_vs_m, 'r')
-        #plt.loglog(mvals, QZ2_vs_m, 'g')
+        plt.loglog(mvals, QZ2_vs_m, 'g')
         plt.loglog(mvals, Q08_vs_m, 'b')
 
 plt.title('Q vs m: all c = '+str(CFID)+'\n '+
@@ -79,21 +81,23 @@ for j in range(NMONTE):
     # Loop over mvalues making indepented submissions at each c, m combination
     for i in range(NBINS):
         g1sub, g2sub = g3utils.make_submission_const_shear(cvals[i], cvals[i], MFID, MFID,
-                                                           g1true, g2true, ngals_per_im=NGALS_PER_IM,
+                                                           g1true, g2true,
+                                                           ngals_per_im=NGALS_PER_IM,
                                                            noise_sigma=NOISE_SIGMA)
-        Q08_vs_c[i] = g3utils.metricQ08_const_shear(g1sub, g2sub, g1true, g2true, ntruesets=NTRUESETS)
+        Q08_vs_c[i] = g3utils.metricQ08_const_shear(g1sub, g2sub, g1true, g2true,
+                                                    ntruesets=NTRUESETS)
         QZ1_vs_c[i] = g3utils.metricQZ1_const_shear(g1sub, g2sub, g1true, g2true,
                                                     cfid=CFID, mfid=MFID)[0]
-        #QZ2_vs_c[i] = g3utils.metricQZ2_const_shear(g1sub, g2sub, g1true, g2true,
-        #                                            cfid=CFID, mfid=MFID)[0]
+        QZ2_vs_c[i] = g3utils.metricQZ2_const_shear(g1sub, g2sub, g1true, g2true,
+                                                    cfid=CFID, mfid=MFID)[0]
 
     if j == 0:
         plt.loglog(cvals, QZ1_vs_c, 'r', label='QZ1')
-        #plt.loglog(cvals, QZ2_vs_c, 'g', label='QZ2')
+        plt.loglog(cvals, QZ2_vs_c, 'g', label='QZ2')
         plt.loglog(cvals, Q08_vs_c, 'b', label='Q08')
     else:
         plt.loglog(cvals, QZ1_vs_c, 'r')
-        #plt.loglog(cvals, QZ2_vs_c, 'g')
+        plt.loglog(cvals, QZ2_vs_c, 'g')
         plt.loglog(cvals, Q08_vs_c, 'b')
 
 plt.title('Q vs c: all m = '+str(MFID)+'\n '+
@@ -105,9 +109,4 @@ outfile = 'Q_vs_c_const_shear_nims'+str(NIMS)+'_nsig'+str(NOISE_SIGMA)+'_truesig
     '.png'
 plt.savefig(outfile)
 plt.show()
-
-
-
-
-
 
