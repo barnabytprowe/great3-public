93ca27a1b50194bce128d839cf2828d84e2a7015
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2013-03-12 18:54:14 -0400

    cleanup test script (#6)

--- galsim_test_pse.py
+++ galsim_test_pse.py
@@ -5,25 +5,64 @@ import matplotlib.pyplot as plt
 
 ### set up basic parameters ###
 # how many times to do this (to beat down noise)
-n_realization = 20
+n_realization = 300
 # file containing theoretical P(k), with fake values added above ell=2000
 pkfile = 'test_pse/ps.wmap7lcdm.2000.dat'
 theory_tab = galsim.LookupTable(file=pkfile, interpolant='linear')
-# name of text file to store results
-outfile = 'test_pse/galsim_test_pse.out'
 # prefix for figures
 figpref = 'test_pse/galsim_test_pse_'
 # number of ell bins used for the estimation
-n_ell = 20
+n_ell = 15
 # N for our grid used for estimating shears
 grid_nx = 100
 # length of grid in one dimension (degrees)
 theta = 10. # degrees
 dtheta = theta/grid_nx
 
-test_ps_e=galsim.PowerSpectrum(e_power_function = pkfile, units='radians')
-test_ps_b=galsim.PowerSpectrum(b_power_function = pkfile, units='radians')
-test_ps_eb=galsim.PowerSpectrum(e_power_function = pkfile, b_power_function = pkfile, units='radians')
+
+def doplot(ell, t, e, b, eb, pref, string, title, rat=None, lim=(1e-7,1e-4)):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(ell, t, label='theory')
+    ax.plot(ell, e, label='Observed EE power')
+    ax.plot(ell, b, label='Observed BB power')
+    ax.plot(ell, eb, label='Observed EB power')
+    plt.ylim(lim)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('ell')
+    ax.set_ylabel('ell*(ell+1)*C_ell/2pi')
+    ax.set_title(title)
+    plt.legend(loc=4)
+    min_ell = 2.*np.pi/(theta*np.pi/180.)
+    max_ell = np.sqrt(2.)*grid_nx*min_ell/2.
+    ax.plot(np.array((min_ell,min_ell)), np.array(lim), color='black')
+    ax.plot(np.array((max_ell,max_ell)), np.array(lim), color='black')
+    figfile = pref + string + '.eps'
+    plt.savefig(figfile)
+    print 'Wrote to file ',figfile
+
+    if rat is not None:
+        ratio = rat/t
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(ell, ratio)
+        plt.ylim(0.5,1.5)
+        ax.set_xscale('log')
+        ax.set_xlabel('ell')
+        ax.set_ylabel('Observed / theory')
+        ax.set_title(title)
+        ax.plot(np.array((min_ell,min_ell)), np.array((0.5,1.5)), color='black')
+        ax.plot(np.array((max_ell,max_ell)), np.array((0.5,1.5)), color='black')
+        ax.plot(np.array((0.5*min_ell,1.5*max_ell)),np.array((1.,1.)),color='black')
+        figfile = pref + string + '.ratio.eps'
+        plt.savefig(figfile)
+        print 'Wrote to file ',figfile
+
+test_ps_e=galsim.PowerSpectrum(e_power_function = theory_tab, units='radians')
+test_ps_b=galsim.PowerSpectrum(b_power_function = theory_tab, units='radians')
+test_ps_eb=galsim.PowerSpectrum(e_power_function = theory_tab,
+                                b_power_function = theory_tab, units='radians')
 
 e_p_e = np.zeros((n_ell, n_realization))
 e_p_b = np.zeros((n_ell, n_realization))
@@ -41,7 +80,7 @@ for ireal in range(n_realization):
     g1, g2 = test_ps_e.buildGriddedShears(grid_spacing=dtheta, ngrid=grid_nx, units=galsim.degrees)
     g2 = -1.*g2 # this might not be necessary, though it was for the GREAT10 PS estimation code
     pse_e = pse.PowerSpectrumEstimator(grid_nx, theta, n_ell)
-    ell, cee_e, cbb_e, ceb_e = pse_e.estimate(g1, g2)
+    ell, cee_e, cbb_e, ceb_e = pse_e.estimate(g1, g2, weight_EE=True)
 
     print "Getting shears on a grid with B power only"
     g1, g2 = test_ps_b.buildGriddedShears(grid_spacing=dtheta, ngrid=grid_nx, units=galsim.degrees)
@@ -53,7 +92,7 @@ for ireal in range(n_realization):
     g1, g2 = test_ps_eb.buildGriddedShears(grid_spacing=dtheta, ngrid=grid_nx, units=galsim.degrees)
     g2 = -1.*g2 # this might not be necessary, though it was for the GREAT10 PS estimation code
     pse_eb = pse.PowerSpectrumEstimator(grid_nx, theta, n_ell)
-    ell, cee_eb, cbb_eb, ceb_eb = pse_eb.estimate(g1, g2)
+    ell, cee_eb, cbb_eb, ceb_eb = pse_eb.estimate(g1, g2, weight_EE=True)
 
     e_p_e[:,ireal] = cee_e
     e_p_b[:,ireal] = cbb_e
@@ -80,64 +119,7 @@ theory_p = np.zeros_like(ell)
 for i in range(n_ell):
     theory_p[i] = theory_tab(ell[i])*ell[i]*(ell[i]+1)/(2.*np.pi)
 
-print "Writing results of test to file ",outfile
-data_all = (ell, theory_p, e_avgp_e, e_avgp_b, e_avgp_eb, b_avgp_e, b_avgp_b, b_avgp_eb, eb_avgp_e, eb_avgp_b, eb_avgp_eb)
-data = np.column_stack(data_all)
-np.savetxt(outfile, data)
-
-
 print "Making figures of dimensionless power, and writing to files"
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(ell,theory_p,label='theory')
-ax.plot(ell,e_avgp_e,label='Observed EE power')
-ax.plot(ell,e_avgp_b,label='Observed BB power')
-ax.plot(ell,e_avgp_eb,label='Observed EB power')
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.set_xlabel('ell')
-ax.set_ylabel('ell*(ell+1)*C_ell/2pi')
-ax.set_title('Input P_E only')
-plt.legend(loc=4)
-ax.plot(np.array((36.,36.)),np.array((1.e-13,1.e-7)),color='black')
-ax.plot(np.array((3600.,3600.)),np.array((1.e-13,1.e-7)),color='black')
-figfile = figpref+'input_pe.eps'
-plt.savefig(figfile)
-
-ratio = e_avgp_e/theory_p
-print ratio
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(ell,theory_p,label='theory')
-ax.plot(ell,b_avgp_e,label='Observed EE power')
-ax.plot(ell,b_avgp_b,label='Observed BB power')
-ax.plot(ell,b_avgp_eb,label='Observed EB power')
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.set_xlabel('ell')
-ax.set_ylabel('ell*(ell+1)*C_ell/2pi')
-ax.set_title('Input P_B only')
-plt.legend(loc=4)
-ax.plot(np.array((36.,36.)),np.array((1.e-13,1.e-7)),color='black')
-ax.plot(np.array((3600.,3600.)),np.array((1.e-13,1.e-7)),color='black')
-figfile = figpref+'input_pb.eps'
-plt.savefig(figfile)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(ell,theory_p,label='theory')
-ax.plot(ell,eb_avgp_e,label='Observed EE power')
-ax.plot(ell,eb_avgp_b,label='Observed BB power')
-ax.plot(ell,eb_avgp_eb,label='Observed EB power')
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.set_xlabel('ell')
-ax.set_ylabel('ell*(ell+1)*C_ell/2pi')
-ax.set_title('Input P_E and P_B')
-plt.legend(loc=4)
-ax.plot(np.array((36.,36.)),np.array((1.e-13,1.e-7)),color='black')
-ax.plot(np.array((3600.,3600.)),np.array((1.e-13,1.e-7)),color='black')
-figfile = figpref+'input_peb.eps'
-plt.savefig(figfile)
-
+doplot(ell, theory_p, e_avgp_e, e_avgp_b, e_avgp_eb, figpref, 'input_pe', 'Input P_E only', rat=e_avgp_e)
+doplot(ell, theory_p, b_avgp_e, b_avgp_b, b_avgp_eb, figpref, 'input_pb', 'Input P_B only', rat=b_avgp_b)
+doplot(ell, theory_p, eb_avgp_e, eb_avgp_b, eb_avgp_eb, figpref, 'input_peb', 'Input P_EB only')
