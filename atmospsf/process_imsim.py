ae997b329b30bd6d412e6164bc9973773e754292
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2013-03-13 22:07:04 -0400

    make a proper e1,e2,dfwhm grid out of imsim inputs (#10)

--- atmospsf/process_imsim.py
+++ atmospsf/process_imsim.py
@@ -29,13 +29,48 @@ def process_file(filename, verbose):
         print "Mean e1, std dev of e1:",np.mean(e1), np.std(e1)
         print "Mean e2, std dev of e2:",np.mean(e2), np.std(e2) 
         print "Showing grid:"
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.plot(x, y, marker='+')
-        ax.set_xlabel('x (arcsec)')
-        ax.set_ylabel('y (arcsec)')
-        ax.set_title('Locations of stars')
-        plt.show()
+        #fig = plt.figure()
+        #ax = fig.add_subplot(111)
+        #ax.plot(x, y, marker='+')
+        #ax.set_xlabel('x (arcsec)')
+        #ax.set_ylabel('y (arcsec)')
+        #plt.xlim((0,1000))
+        #plt.ylim((0,1000))
+        #ax.set_title('Locations of stars')
+        #plt.show()
+
+    # Now define expected grid locations
+    x_grid_min = 200.
+    y_grid_min = 48.
+    dgrid = 360. # 10 degrees / 100
+    ngrid = 21 # because this grid is only covering 2 degrees
+    x_grid_index = (x-x_grid_min)/dgrid
+    round_x_grid_index = np.round(x_grid_index)
+    y_grid_index = (y-y_grid_min)/dgrid
+    round_y_grid_index = np.round(y_grid_index)
+    x_use = round_x_grid_index[(np.abs(round_x_grid_index-x_grid_index)<0.05) & (np.abs(round_y_grid_index-y_grid_index)<0.05)].astype(np.int)
+    y_use = round_y_grid_index[(np.abs(round_x_grid_index-x_grid_index)<0.05) & (np.abs(round_y_grid_index-y_grid_index)<0.05)].astype(np.int)
+    e1_use = e1[(np.abs(round_x_grid_index-x_grid_index)<0.05) & (np.abs(round_y_grid_index-y_grid_index)<0.05)].astype(np.int)
+    e2_use = e2[(np.abs(round_x_grid_index-x_grid_index)<0.05) & (np.abs(round_y_grid_index-y_grid_index)<0.05)].astype(np.int)
+    dfwhm_use = dfwhm[(np.abs(round_x_grid_index-x_grid_index)<0.05) & (np.abs(round_y_grid_index-y_grid_index)<0.05)].astype(np.int)
+    if verbose:
+        print "After grid selection:"
+        print "Number of grid points:",len(x_use)
+        print "Min,max x,y:",np.min(x_use), np.max(x_use), np.min(y_use), np.max(y_use)
+        #fig = plt.figure()
+        #ax = fig.add_subplot(111)
+        #ax.plot(x_use, y_use, marker='+')
+        #ax.set_xlabel('x index')
+        #ax.set_ylabel('y index')
+        #plt.show()
+
+    # Make grids of the interesting parameters.
+    e1_grid = np.zeros((ngrid, ngrid))
+    e2_grid = np.zeros((ngrid, ngrid))
+    dfwhm_grid = np.zeros((ngrid, ngrid))
+    e1_grid[x_use, y_use] = e1_use
+    e2_grid[x_use, y_use] = e2_use
+    dfwhm_grid[x_use, y_use] = dfwhm_use
 
 # Main function: for now, just specify a single file directly.
 infile = '/Users/rmandelb/great3/chihway-sims/catalogs/1min/focalplane_47.txt'
