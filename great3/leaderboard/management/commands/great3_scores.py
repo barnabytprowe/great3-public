3ab34d4a6a8f92d019dadc1ca4f17567b82d7c87
Joe Zuntz <joezuntz@googlemail.com>
2013-04-17 15:34:21 +0100

    Added logging of score calculations

--- great3/leaderboard/management/commands/great3_scores.py
+++ great3/leaderboard/management/commands/great3_scores.py
@@ -1,12 +1,19 @@
 from django.core.management.base import BaseCommand, CommandError
 from leaderboard.models import Entry, PLACEHOLDER_SCORE, recompute_scoring
+from great3.settings import installation_base
 import numpy as np
+import os
+import datetime
+import pytz
+
+SCORE_LOG_FILE = os.path.join(installation_base, "results", "results.log")
 
 class Command(BaseCommand):
 	args = ''
 	help = 'Checks for entries that do not have a score yet and processes them'
 
 	def handle(self, *args, **options):
+		outfile = open(SCORE_LOG_FILE,'a')
 		entries = Entry.objects.filter(score=PLACEHOLDER_SCORE)
 		for entry in entries:
 			print "Processing entry %s" % entry.name
@@ -14,5 +21,8 @@ class Command(BaseCommand):
 			print "should compute score here, from loading entry filename ", filename
 			print "Instead using random number from 1-1000"
 			entry.score = np.random.uniform(1, 1000)
+			datestamp = datetime.datetime.utcnow().replace(tzinfo=pytz.utc).isoformat()
+			outfile.write('%s\t%s\t%r\t%s\n' % (entry.name,filename,entry.score,datestamp))
 			entry.save()
+		outfile.close()
 		recompute_scoring()
