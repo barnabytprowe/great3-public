5478610f03326ab806d5a4c5194266edabc10083
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2013-04-30 00:17:42 -0400

    bug fixes, optimizations for tests of atmospheric P(k) (#10)

--- atmospsf/test_atmos_pk.py
+++ atmospsf/test_atmos_pk.py
@@ -6,7 +6,7 @@ import os
 import subprocess
 
 # This class defines the integrand for when we are trying to get
-#   P(k) = (1/2pi) \int theta dtheta J_0(k theta) xi_+(theta).
+#   P(k) = 2pi \int theta dtheta J_0(k theta) xi_+(theta).
 # We have a special functional form for xi(theta), which is
 #   xi_+(theta) = amp / (1 + theta/theta_0).
 # This means we can store amp and theta, and evaluate the integrand for arbitrary theta, without
@@ -25,7 +25,7 @@ def corrfunc_to_ps(amp, theta_0, k, max_r):
     # We are trying to get P(k) for our atmospheric PSF, which has the special property that xi_-=0,
     # implying P_E(k) = P_B(k).  Thus, there is some P(k) = P_E(k) + P_B(k) which relates to xi_+
     # via
-    # P(k) = (1/2pi) \int theta dtheta J_0(k theta) xi_+(theta).
+    # P(k) = 2pi \int theta dtheta J_0(k theta) xi_+(theta).
     # We have a special functional form for xi(theta), which is
     #   xi_+(theta) = amp / (1 + theta/theta_0).
     # Later, we will have to take this P(k), and when using the galsim lensing engine, assign P_E =
@@ -36,8 +36,8 @@ def corrfunc_to_ps(amp, theta_0, k, max_r):
     for k_ind in range(len(k)):
         integrand = p_integrand(theta_0, k[k_ind])
         p[k_ind] = galsim.integ.int1d(integrand, 0., max_r,
-                                      rel_err=1.e-4, abs_err=1.e-8)
-    p *= amp/(2.*np.pi)
+                                      rel_err=1.e-3, abs_err=1.e-6)
+    p *= 2.*np.pi*amp
     return p
 
 def run_corr2_ascii(x,y,e1,e2,min_sep,max_sep,nbins):
@@ -75,11 +75,11 @@ k_max = np.sqrt(2.)*np.pi/dtheta_arcsec
 test_k = np.logspace(np.log10(k_min), np.log10(k_max), num=20)
 amp = 1.e-4
 print "To PS, 1"
-p1 = corrfunc_to_ps(amp, 100., test_k, 2*theta_arcsec)
+p1 = corrfunc_to_ps(amp, 100., test_k, 2.5*theta_arcsec)
 print "To PS, 2"
-p2 = corrfunc_to_ps(amp, 1000., test_k, 2*theta_arcsec)
+p2 = corrfunc_to_ps(amp, 1000., test_k, 2.5*theta_arcsec)
 print "To PS, 3"
-p3 = corrfunc_to_ps(amp, 3000., test_k, 2*theta_arcsec)
+p3 = corrfunc_to_ps(amp, 3000., test_k, 4*theta_arcsec)
 print "Plotting"
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -96,8 +96,8 @@ pk_arcsec = pk_data[1]*(rad_to_arcsec**2)
 ax.plot(k_arcsec, pk_arcsec, label='cosmic shear, zmed=1')
 ell_20 = 20./rad_to_arcsec
 ell_2000 = 2000./rad_to_arcsec
-ax.plot((ell_20, ell_20), (1.e-10,1.e5), label='ell=20')
-ax.plot((ell_2000, ell_2000), (1.e-10,1.e5), label='ell=2000')
+ax.plot((ell_20, ell_20), (1.e-4,1.e5), label='ell=20')
+ax.plot((ell_2000, ell_2000), (1.e-4,1.e5), label='ell=2000')
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_xlabel('k [1/arcsec]')
@@ -106,46 +106,72 @@ plt.legend()
 print "Saving plot to file"
 plt.savefig('diagnostics/test_atmos_pk.jpg')
 
-# Take the outputs and run Mike's corr2 program to check that the outputs correspond to
-# inputs.  For now, let's just use the theta0=100 arcsec case.  Use 0.5*P but give that to both P_E
-# and P_B.
-print "getting shears using GalSim"
-# some kludginess to avoid neg numbers, did check that this is not very bad
-new_p = 0.5*p1-np.min(0.5*p1)+1.e-12
-tab_pk = galsim.LookupTable(test_k, new_p, x_log=True, f_log=True)
-ps = galsim.PowerSpectrum(tab_pk, tab_pk, units=galsim.arcsec)
-# Note, typically this returns shear, not e.  For nearly round things, e~2*shear.  But for the
-# atmospheric PSF stuff, I gave the code amplitudes corresponding to some ellipticity variance, so
-# the results should correspond to e1/e2 in amplitude as well.  The alternative approach would have
-# been to use shear variances (factor of 4 lower) and then to take the e that come out and multiply
-# by 2.  This seemed silly so I didn't do it.  However, it does mean that the kappa fluctuations are
-# too high by a factor of 4, so I must reduce them.
-e1, e2, kappa = ps.buildGrid(grid_spacing=dtheta_arcsec,
-                             ngrid=ngrid/oversample,
-                             get_convergence=True)
-grid_range = (dtheta_arcsec) * np.arange(ngrid/oversample)
-x, y = np.meshgrid(grid_range, grid_range)
-print np.min(x), np.max(x), np.min(y), np.max(y)
-kappa /= 4.
-var1=np.var(e1)
-var2=np.var(e2)
-vark=np.var(kappa)
-print 'Variances:',var1,var2,var1+var2,vark
-results = run_corr2_ascii(x,y,e1,e2,dtheta_arcsec,np.sqrt(2)*dtheta_arcsec*ngrid/oversample,ngrid/oversample)
-theta_corr2 = results[:,1]
-xip_corr2 = results[:,2]
-xim_corr2 = results[:,3]
+# the shear part is expensive, so let's not do it every time, particularly if I just want to mess
+# with the P(k) calculation.
+do_shear = True
+# also make an option to just use some of the grid, to save time
+use_subgrid = True
+if do_shear:
+    # Take the outputs and run Mike's corr2 program to check that the outputs correspond to
+    # inputs.  For now, let's just use the theta0=100 arcsec case.  Use 0.5*P but give that to both P_E
+    # and P_B.
+    print "getting shears using GalSim"
+    # some kludginess to avoid neg numbers, but I did check that this is not very bad
+    new_p = 0.5*p1-np.min(0.5*p1)+1.e-12
+    tab_pk = galsim.LookupTable(test_k, new_p, x_log=True, f_log=True)
+    ps = galsim.PowerSpectrum(tab_pk, tab_pk, units=galsim.arcsec)
+    # Note, typically this returns shear, not e.  For nearly round things, e~2*shear.  But for the
+    # atmospheric PSF stuff, I gave the code amplitudes corresponding to some ellipticity variance, so
+    # the results should correspond to e1/e2 in amplitude as well.  The alternative approach would have
+    # been to use shear variances (factor of 4 lower) and then to take the e that come out and multiply
+    # by 2.  This seemed silly so I didn't do it.  However, it does mean that the kappa fluctuations are
+    # too high by a factor of 4, so I must reduce them.
+    e1, e2, kappa = ps.buildGrid(grid_spacing=dtheta_arcsec*subsample,
+                                 ngrid=ngrid/subsample,
+                                 get_convergence=True)
+    kappa /= 4.
+    if not use_subgrid:
+        grid_range = (dtheta_arcsec*subsample) * np.arange(ngrid/subsample)
+        x, y = np.meshgrid(grid_range, grid_range)
+        results = run_corr2_ascii(x,y,e1,e2,dtheta_arcsec*subsample,np.sqrt(2)*dtheta_arcsec*ngrid,ngrid/subsample)
+        theta_corr2 = results[:,1]
+        xip_corr2 = results[:,2]
+        xim_corr2 = results[:,3]    
+    else:
+        n_iter = 0
+        grid_range = (dtheta_arcsec*subsample) * np.arange(ngrid/(subsample*oversample))
+        x, y = np.meshgrid(grid_range, grid_range)
+        for x_ind in range(oversample):
+            x_start = x_ind*ngrid/(subsample*oversample)
+            x_end = (x_ind+1)*ngrid/(subsample*oversample)
+            for y_ind in range(oversample):
+                print "  Subsample ",n_iter
+                y_start = y_ind*ngrid/(subsample*oversample)
+                y_end = (y_ind+1)*ngrid/(subsample*oversample)
+                e1_sub = e1[x_start:x_end]
+                e2_sub = e2[y_start:y_end]
+                results = run_corr2_ascii(x,y,e1_sub,e2_sub,dtheta_arcsec*subsample,np.sqrt(2)*dtheta_arcsec*ngrid/oversample,ngrid/(subsample*oversample))
+                if n_iter == 0:
+                    theta_corr2 = results[:,1]
+                    xip_corr2 = results[:,2]
+                    xim_corr2 = results[:,3]
+                else:
+                    xip_corr2 += results[:,2]
+                    xim_corr2 += results[:,3]
+                n_iter += 1
+        xip_corr2 /= oversample**2
+        xim_corr2 /= oversample**2
 
-print "Plotting ellip corr func"
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(theta_corr2, xip_corr2, label='observed xi+')
-ax.plot(theta_corr2, xim_corr2, label='observed xi-')
-ax.plot(theta_corr2, 1.e-4/(1+theta_corr2/100.), label='input xi+')
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.set_xlabel('theta [arcsec]')
-ax.set_ylabel('xi')
-plt.legend()
-print "Saving plot to file"
-plt.savefig('diagnostics/test_atmos_pk_xi.jpg')
+    print "Plotting ellip corr func"
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(theta_corr2, xip_corr2, label='observed xi+')
+    ax.plot(theta_corr2, xim_corr2, label='observed xi-')
+    ax.plot(theta_corr2, 1.e-4/(1+theta_corr2/100.), label='input xi+')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('theta [arcsec]')
+    ax.set_ylabel('xi')
+    plt.legend()
+    print "Saving plot to file"
+    plt.savefig('diagnostics/test_atmos_pk_xi.jpg')
