8ed245786e0236807573ff10903940cf110528b8
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-06-01 19:24:16 +0100

    added scripts and results from a trial with the same PS realization each time - no noticeable change in results (#12)

--- plot_PS_normalization_results.py
+++ plot_PS_normalization_results.py
@@ -15,11 +15,11 @@ ctest = calcnorm.CTEST
 mtest = calcnorm.MTEST
 
 plt.clf()
-if not os.path.isdir('plots'):
-    os.mkdir('plots')
+if not os.path.isdir(os.path.join('plots', 'v3')):
+    os.mkdir(os.path.join('plots', 'vs'))
 
 # First plot the histograms of QG10 for each of the ctest values for m = mtest[0] (fiducial)
-plotfile1 = os.path.join('plots', 'normalization_QG10_vs_c.png')
+plotfile1 = os.path.join('plots', 'v3', 'normalization_QG10_vs_c.png')
 for c, i in zip(ctest, range(len(ctest))):
     plt.hist(qG10unnorm[i, 0, :].flatten(), bins=30, range=(0., 6.e7), label=r'c$_i$='+str(c))
 plt.xlabel(r'QG10 (unnormalized fiducial m$_i$='+str(mtest[0])+')')
@@ -27,7 +27,7 @@ plt.legend()
 plt.savefig(plotfile1)
 
 # Then plot the histograms of QQuadPS for each of the ctest values for m = mtest[0] (fiducial)
-plotfile2 = os.path.join('plots', 'normalization_QQuadPS_vs_c.png')
+plotfile2 = os.path.join('plots', 'v3', 'normalization_QQuadPS_vs_c.png')
 plt.clf()
 for c, i in zip(ctest, range(len(ctest))):
     plt.hist(qQuadPSunnorm[i, 0, :].flatten(), bins=30, range=(0., 6.e7), label=r'c$_i$='+str(c))
@@ -45,7 +45,7 @@ qQuadPS = 1000. * qQuadPSunnorm / normQQuadPS
 # Then plot histograms of normalized values as a function of m
 # QG10
 plt.clf()
-plotfile3 = os.path.join('plots', 'hists_QG10_vs_m.png')
+plotfile3 = os.path.join('plots', 'v3', 'hists_QG10_vs_m.png')
 for m, j in zip(mtest, range(len(mtest))):
     plt.hist(qG10[:, j, :].flatten(), bins=75, range=(0., 2500.), label=r'm$_i$='+str(m))
 plt.legend()
@@ -54,7 +54,7 @@ plt.savefig(plotfile3)
 
 # QQuadPS
 plt.clf()
-plotfile4 = os.path.join('plots', 'hists_QQuadPS_vs_m.png')
+plotfile4 = os.path.join('plots', 'v3', 'hists_QQuadPS_vs_m.png')
 for m, j in zip(mtest, range(len(mtest))):
     plt.hist(qQuadPS[:, j, :].flatten(), bins=75, range=(0., 2500.), label=r'm$_i$='+str(m))
 plt.legend()
@@ -64,7 +64,7 @@ plt.savefig(plotfile4)
 # Then plot means and standard deviations of these histograms
 # QG10
 plt.clf()
-plotfile5 = os.path.join('plots', 'QG10_vs_m.png')
+plotfile5 = os.path.join('plots', 'v3', 'QG10_vs_m.png')
 meanQG10 = []
 stdQG10 = []
 for m, j in zip(mtest, range(len(mtest))):
@@ -78,7 +78,7 @@ plt.savefig(plotfile5)
 
 # QQuadPS
 plt.clf()
-plotfile6 = os.path.join('plots', 'QQuadPS_vs_m.png')
+plotfile6 = os.path.join('plots', 'v3', 'QQuadPS_vs_m.png')
 meanQQuadPS = []
 stdQQuadPS = []
 for m, j in zip(mtest, range(len(mtest))):
@@ -89,6 +89,4 @@ plt.xlim(-.5, -3.5)
 plt.xlabel(r'log$_{10}$(m$_i$)')
 plt.ylabel('QQuadPS')
 plt.savefig(plotfile6)
-
-
 
