e7aaceb92fab0f6405ee9892d289632c84418498
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-07-09 01:14:30 +0100

    added plotting routines to const shear metric code (#12)

--- calculate_plot_const_shear_metrics.py
+++ calculate_plot_const_shear_metrics.py
@@ -20,7 +20,7 @@ MMIN = MFID
 MMAX = 2.e-1
 
 NBINS = 5     # Number of bins to plot in the ranges above
-NMONTE = 100  # Number of montecarlo samples
+NMONTE = 100  # Number of montecarlo realizations
 NOISE_SIGMA = 0.05  # Noise due to pixel shot noist on a shear estimate, per galaxy
 
 # Generate arrays of values
@@ -36,35 +36,115 @@ QZ2 = np.empty((NBINS, NBINS, NMONTE))
 # File for storing pickled output
 OUTFILE = os.path.join('results', 'const_shear_metrics.pkl')
 
-if not os.path.isfile(OUTFILE):
-    for krepeat in range(NMONTE):
+if __name__ == "__main__":
 
-        # Generate the truth tables for this realization
-        g1true, g2true = g3metrics.make_const_truth_uniform_annular(
-            NFIELDS, NIMS, range_min=TRUE_MIN, range_max=TRUE_MAX)
+    # Load up the outfile for plotting, or calculate and save before plotting
+    if os.path.isfile(OUTFILE):
+        Q08, QZ1, QZ2 = cPickle.load(open(OUTFILE, 'rb'))
+
+    else:
+        for krepeat in range(NMONTE):
+
+            # Generate the truth tables for this realization
+            g1true, g2true = g3metrics.make_const_truth_uniform_annular(
+                NFIELDS, NIMS, range_min=TRUE_MIN, range_max=TRUE_MAX)    
+            # Loop over each c, m combination
+            for i in range(NBINS):
+                for j in range(NBINS):
+                    # Make the submissions
+                    g1sub, g2sub = g3metrics.make_submission_const_shear(
+                        cvals[i], cvals[i], mvals[j], mvals[j], g1true, g2true,
+                        ngals_per_im=NGALS_PER_IM, noise_sigma=NOISE_SIGMA)
+                    # Calculate the metrics and store
+                    Q08[i, j, krepeat] = g3metrics.metricQ08_const_shear(
+                        g1sub, g2sub, g1true, g2true, nfields=NFIELDS)
+                    QZ1[i, j, krepeat] = g3metrics.metricQZ1_const_shear(
+                        g1sub, g2sub, g1true, g2true, cfid=CFID, mfid=MFID)[0]
+                    QZ2[i, j, krepeat] = g3metrics.metricQZ2_const_shear(
+                        g1sub, g2sub, g1true, g2true, cfid=CFID, mfid=MFID)[0]
+
+            print "Calculated const shear metrics for "+str(krepeat + 1)+"/"+\
+                str(NMONTE)+" realizations"
+
+        # Save the results as a tuple of NumPy arrays
+        cPickle.dump((Q08, QZ1, QZ2), open(OUTFILE, 'wb'))
+
+    # Get basic statistics
+    q08mean = np.mean(Q08, axis=2)
+    qZ1mean = np.mean(QZ1, axis=2)
+    qZ2mean = np.mean(QZ2, axis=2)
+    q08stds = np.std(Q08, axis=2)
+    qZ1stds = np.std(QZ1, axis=2)
+    qZ2stds = np.std(QZ2, axis=2)
+    q08errs = q08stds / np.sqrt(NMONTE)
+    qZ1errs = qZ1stds / np.sqrt(NMONTE)
+    qZ2errs = qZ2stds / np.sqrt(NMONTE)
+
+    # Normalise QZs to to Q=1000 for fiducial case (should be approx OK, but might be biased)
+    qZ1norm = 1000. / qZ1mean[0, 0]
+    qZ2norm = 1000. / qZ2mean[0, 0]
+    qZ1norm_err = qZ1norm * (qZ1errs[0, 0] / qZ1mean[0, 0])
+    qZ2norm_err = qZ2norm * (qZ2errs[0, 0] / qZ2mean[0, 0])
+    print ("Normalization factor for QZ1 = %.3f +/- %.3f" % (qZ1norm, qZ1norm_err))
+    print ("Normalization factor for QZ2 = %.3f +/- %.3f" % (qZ2norm, qZ2norm_err))
+    qZ1mean *= qZ1norm
+    qZ2mean *= qZ2norm
     
-        # Loop over each c, m combination
-        for i in range(NBINS):
-        
-            for j in range(NBINS):
+    # Plot the Q metric and the standard deviation of the NMONTE results, versus c first
+    import matplotlib.pyplot as plt
+    # QZ1
+    plt.clf()
+    plt.axhline(ls='--', color='k')
+    for i in range(len(mvals)):
+        plt.errorbar(
+            cvals * (1.00**i), qZ1mean[:, i], yerr=qZ1stds[:, i], label='m = %.0e' % mvals[i])
+    plt.xscale('log')
+    plt.xlim(3.e-2, 2.e-5)
+    plt.legend()
+    plt.title('Mean QZ1 metric and standard deviation of '+str(NMONTE)+' realizations')
+    plt.ylabel('Q')
+    plt.xlabel('c')
+    plt.savefig(os.path.join('plots', 'const_QZ1_vs_c_N'+str(NMONTE)+'.png'))
 
-                # Make the submissions
-                g1sub, g2sub = g3metrics.make_submission_const_shear(
-                    cvals[i], cvals[i], mvals[j], mvals[j], g1true, g2true,
-                    ngals_per_im=NGALS_PER_IM, noise_sigma=NOISE_SIGMA)
-                # Calculate the metrics and store
-                Q08[i, j, krepeat] = g3metrics.metricQ08_const_shear(
-                    g1sub, g2sub, g1true, g2true, nfields=NFIELDS)
-                QZ1[i, j, krepeat] = g3metrics.metricQZ1_const_shear(
-                    g1sub, g2sub, g1true, g2true, cfid=CFID, mfid=MFID)[0]
-                QZ2[i, j, krepeat] = g3metrics.metricQZ2_const_shear(
-                    g1sub, g2sub, g1true, g2true, cfid=CFID, mfid=MFID)[0]
+    # QZ2
+    plt.clf()
+    plt.axhline(ls='--', color='k')
+    for i in range(len(mvals)):
+        plt.errorbar(
+            cvals * (1.00**i), qZ2mean[:, i], yerr=qZ2stds[:, i], label='m = %.0e' % mvals[i])
+    plt.xscale('log')
+    plt.xlim(3.e-2, 2.e-5)
+    plt.legend()
+    plt.title('Mean QZ2 metric and standard deviation of '+str(NMONTE)+' realizations')
+    plt.ylabel('Q')
+    plt.xlabel('c')
+    plt.savefig(os.path.join('plots', 'const_QZ2_vs_c_N'+str(NMONTE)+'.png'))
 
-        print "Calculated const shear metrics for "+str(krepeat + 1)+"/"+str(NMONTE)+" realizations"
+    # Plot the Q metric and the standard deviation of the NMONTE results, versus m
+    # QZ1
+    plt.clf()
+    plt.axhline(ls='--', color='k')
+    for i in range(len(cvals)):
+        plt.errorbar(
+            mvals * (1.00**i), qZ1mean[i, :], yerr=qZ1stds[i, :], label='c = %.0e' % cvals[i])
+    plt.xscale('log')
+    plt.xlim(3.e-1, 2.e-4)
+    plt.legend()
+    plt.title('Mean QZ1 metric and standard deviation of '+str(NMONTE)+' realizations')
+    plt.ylabel('Q')
+    plt.xlabel('m')
+    plt.savefig(os.path.join('plots', 'const_QZ1_vs_m_N'+str(NMONTE)+'.png'))
 
-    # Save the results as a tuple of NumPy arrays
-    cPickle.dump((Q08, QZ1, QZ2), open(OUTFILE, 'wb'))
-
-else:
-    Q08, QZ1, QZ2 = cPickle.load(open(OUTFILE, 'rb'))
-
+    # QZ2
+    plt.clf()
+    plt.axhline(ls='--', color='k')
+    for i in range(len(cvals)):
+        plt.errorbar(
+            mvals * (1.00**i), qZ2mean[i, :], yerr=qZ2stds[i, :], label='c = %.0e' % cvals[i])
+    plt.xscale('log')
+    plt.xlim(3.e-1, 2.e-4)
+    plt.legend()
+    plt.title('Mean QZ2 metric and standard deviation of '+str(NMONTE)+' realizations')
+    plt.ylabel('Q')
+    plt.xlabel('m')
+    plt.savefig(os.path.join('plots', 'const_QZ2_vs_m_N'+str(NMONTE)+'.png'))
