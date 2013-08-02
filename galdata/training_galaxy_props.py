a52eadf3c5a6ba8b7656751d3a59761b49078d03
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2013-08-02 14:07:45 -0400

    significant revamping of script to get training galaxy properties (#42)

--- galdata/training_galaxy_props.py
+++ galdata/training_galaxy_props.py
@@ -4,6 +4,7 @@ import galsim
 import pyfits
 import os
 import numpy as np
+import math
 
 def training_galaxy_props(psf,
                           in_dir = '.',
@@ -58,78 +59,86 @@ def training_galaxy_props(psf,
     flux_frac = np.zeros(n)
     resolution = np.zeros(n)
     noise_var_snr_20 = np.zeros(n)
+    use_bulgefit = np.ones(n)
     for i in range(n):
         if i % 1000 == 0:
             print "...",i
         params = dat[i].field('bulgefit')
+        sparams = dat[i].field('sersicfit')
+        bstat = dat[i].field('fit_status')[0]
+        sstat = dat[i].field('fit_status')[4]
+        dvc_btt = dat[i].field('fit_dvc_btt')
+        bmad = dat[i].field('fit_mad_b')
+        smad = dat[i].field('fit_mad_s')
 
-        bulge_q = params[11]
-        disk_q = params[3]
-        if bulge_q < 0 or bulge_q > 1 or disk_q < 0 or disk_q > 1:
-            do_meas[i] = -1 # indicates fit parameters are nonsense
-            resolution[i] = -10.0
-            noise_var_snr_20[i] = -10.0
-            flux_frac[i] = -10.0
+    if bstat<1 or bstat>4 or dvc_btt<0.1 or dvc_btt>0.9 or np.isnan(dvc_btt) or params[9]<=0 or params[1]<=0 or params[11]<0.051 or params[3]<0.051 or smad<bmad:
+        use_bulgefit[i] = 0
+        # Then check if sersicfit is viable; if not, this object is a total failure:
+        if sstat<1 or sstat>4 or sparams[1]<=0 or sparams[0]<=0:
+            use_bulgefit[i] = -1
+            do_meas[i] = -1
+            e1[i] = -10.
+            e2[i] = -10.
+            bt[i] = -10.
             continue
 
+    if use_bulgefit[i]:
+        bulge_q = params[11]
         bulge_beta = params[15]*galsim.radians
-        bulge_hlr = 0.03*size_factor*np.sqrt(bulge_q)*params[9]
+        bulge_hlr = 0.03*np.sqrt(bulge_q)*params[9]
         bulge_flux = 2.0*np.pi*3.607*(bulge_hlr**2)*params[8]
 
+        disk_q = params[3]
         disk_beta = params[7]*galsim.radians
-        disk_hlr = 0.03*size_factor*np.sqrt(disk_q)*params[1]
+        disk_hlr = 0.03*np.sqrt(disk_q)*params[1]
         disk_flux = 2.0*np.pi*1.901*(disk_hlr**2)*params[0]
-
+        
         bfrac = bulge_flux/(bulge_flux+disk_flux)
-        bt[i] = bfrac
-        if bt[i] < 0 or bt[i] > 1 or np.isnan(bt[i]):
-            do_meas[i] = -1 # indicates fit parameters are nonsense
-            resolution[i] = -10.0
-            noise_var_snr_20[i] = -10.0
-            flux_frac[i] = -10.0
-            continue
-        elif bfrac > 0.07 and bfrac < 0.95:
-            bulge = galsim.Sersic(4.0, half_light_radius = bulge_hlr, flux = bulge_flux)
-            disk = galsim.Exponential(half_light_radius = disk_hlr, flux = disk_flux)
-            if bulge_q > 0.:
-                bulge.applyShear(q = bulge_q, beta = bulge_beta)
-            if disk_q > 0.:
-                disk.applyShear(q = disk_q, beta = disk_beta)
-            gal = bulge+disk
-        elif bfrac <= 0.07:
-            disk = galsim.Exponential(half_light_radius = disk_hlr, flux = disk_flux)
-            if disk_q > 0.:
-                disk.applyShear(q = disk_q, beta = disk_beta)
-            gal = disk
-        else:
-            bulge = galsim.Sersic(4.0, half_light_radius = bulge_hlr, flux = bulge_flux)
-            if bulge_q > 0.:
-                bulge.applyShear(q = bulge_q, beta = bulge_beta)
-            gal = bulge
 
-        # get basic statistics
-        obj = galsim.Convolve(gal, epsf, gsparams = galsim.GSParams(maximum_fft_size=15000))
-        im = galsim.ImageF(ps_size, ps_size)
-        try:
-            im = obj.draw(dx = pix_scale)
-        except:
-            do_meas[i] = -0.5 # indicates fit parameters make object impossible-to-draw
-            resolution[i] = -10.0
-            noise_var_snr_20[i] = -10.0
-            flux_frac[i] = -10.0
+        if bfrac < 0 or bfrac > 1 or np.isnan(bfrac):
+            e1[i] = -10.
+            e2[i] = -10.
+            do_meas[i] = -1
+            use_bulgefit[i] = -1
             continue
-            
-        flux_frac[i] = im.array.sum()/(bulge_flux+disk_flux)
-        noise_var_snr_20[i] = np.sum(im.array**2) / 20.**2
+
+        bt[i] = bfrac
+        bulge = galsim.Sersic(4.0, half_light_radius = bulge_hlr, flux = bulge_flux)
+        disk = galsim.Exponential(half_light_radius = disk_hlr, flux = disk_flux)
+        if bulge_q > 0.:
+            bulge.applyShear(q = bulge_q, beta = bulge_beta)
+        if disk_q > 0.:
+            disk.applyShear(q = disk_q, beta = disk_beta)
+        gal = bulge+disk
+        gal_flux = bulge_flux + disk_flux
+
+    else:
+        gal_n = sparams[2]
+        tmp_ser = galsim.Sersic(gal_n, half_light_radius=1.)
+        gal_bn = (1./tmp_ser.getScaleRadius())**(1./gal_n)
+        prefactor = gal_n * math.gamma(2.*gal_n) * math.exp(gal_bn) / (gal_bn**(2.*gal_n))
+        gal_q = sparams[3]
+        gal_beta = sparams[7]*galsim.radians
+        gal_hlr = 0.03*np.sqrt(gal_q)*sparams[1]
+        gal_flux = 2.*np.pi*prefactor*(gal_hlr**2)*params[0]
+
+        gal = galsim.Sersic(gal_n, half_light_radius = gal_hlr, flux = gal_flux)
+        if gal_q > 0.:
+            gal.applyShear(q = gal_q, beta = gal_beta)
+
+    obj = galsim.Convolve(gal, epsf, gsparams = galsim.GSParams(maximum_fft_size=15000))
+    im = galsim.ImageF(ps_size, ps_size)
+    flux_frac[i] = im.array.sum()/gal_flux
+    noise_var_snr_20[i] = np.sum(im.array**2) / 20.**2
 
         # try getting resolution
-        try:
-            result = galsim.hsm.EstimateShear(im, im_epsf, guess_sig_gal=20)
-            resolution[i] = result.resolution_factor
-            do_meas[i] = 1. # made model and was able to measure shape
-        except RuntimeError:
-            resolution[i] = -10.
-            do_meas[i] = 0. # made model and did other calculations, but could not measure shape
+    try:
+        result = galsim.hsm.EstimateShear(im, im_epsf, guess_sig_gal=20)
+        resolution[i] = result.resolution_factor
+        do_meas[i] = 1. # made model and was able to measure shape
+    except RuntimeError:
+        resolution[i] = -10.
+        do_meas[i] = 0. # made model and did other calculations, but could not measure shape
 
     # save results to file
     tbhdu = pyfits.new_table(pyfits.ColDefs([pyfits.Column(name='IDENT',
@@ -149,11 +158,16 @@ def training_galaxy_props(psf,
                                                            array=noise_var_snr_20),
                                              pyfits.Column(name='do_meas',
                                                            format='D',
-                                                           array=do_meas)]
+                                                           array=do_meas),
+                                             pyfits.Column(name='use_bulgefit',
+                                                           format='D',
+                                                           array=use_bulgefit)]
                                             ))
     
     fail_ind = np.where(do_meas < -0.5)[0]
     print len(fail_ind),' failures'
+    bulgefit_ind = np.where(use_bulgefit == 1)[0]
+    print len(bulgefit_ind),' use 2-component fits'
 
     # write outputs
     outfile = os.path.join(out_dir, out_filename)
