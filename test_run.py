9ee7531ef88ece4310b741be892cb42645cc0200
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2013-09-03 19:22:00 -0400

    make config steps optional in test_run.py, since config stuff doesn't work yet for variable_psf (#51)

--- test_run.py
+++ test_run.py
@@ -13,18 +13,18 @@ import great3
 # Set which branches to test...
 experiments = [
     'variable_psf',
-    #'control',
-    #'multiepoch',
+    'control',
+    'multiepoch',
     #'real_gal',
     #'full',
 ]
 obs_type = [
     'ground',
-#    'space',
+    'space',
 ]
 shear_type = [
     'constant',
-#    'variable',
+    'variable',
 ]
 
 root = 'test_run'
@@ -34,6 +34,8 @@ subfield_max = 5    # Total of 3x2 sub-fields  (2 per config file)
 data_dir = 'great3_fit_data'    # This should be set up as a sim-link to your Dropbox folder.
 ps_dir = '../inputs/ps/tables' 
 seed = 12345                    # Whatever.  (But not zero.)
+do_config = False   # Do config-related steps?  For now, this is False, because they are not set up
+                    # properly for variable PSF branches.
 
 # Clean up possible residue from previous runs.
 shutil.rmtree(root, ignore_errors=True)
@@ -76,66 +78,67 @@ for exp in experiments:
                 n_epochs.append(1)
 
 # Build config files
-t1 = time.time()
-new_config_names = []
-new_psf_config_names = []
-for i in range(n_config):
-    first = subfield_min + (subfield_max-subfield_min+1)/n_config * i
-    last = subfield_min + (subfield_max-subfield_min+1)/n_config * (i+1) - 1
-    great3.run(root, subfield_min=first, subfield_max=last,
-            experiments=experiments, obs_type=obs_type, shear_type=shear_type,
-            gal_dir=data_dir, ps_dir=ps_dir,
-            seed=seed, steps=['config']
-    )
-    for config_name in config_names:
-        name, ext = os.path.splitext(config_name)
-        new_name = '%s_%02d.yaml'%(name,i)
-        new_config_names.append(new_name)
-        shutil.copy(os.path.join(root,config_name),os.path.join(root,new_name))
-    for config_name in psf_config_names:
-        name, ext = os.path.splitext(config_name)
-        new_name = '%s_%02d_psf.yaml'%(name,i)
-        new_psf_config_names.append(new_name)
-        shutil.copy(os.path.join(root,config_name),os.path.join(root,new_name))
-t2 = time.time()
-print
-print 'Time for great3.run config = ',t2-t1
-print
+if do_config:
+    t1 = time.time()
+    new_config_names = []
+    new_psf_config_names = []
+    for i in range(n_config):
+        first = subfield_min + (subfield_max-subfield_min+1)/n_config * i
+        last = subfield_min + (subfield_max-subfield_min+1)/n_config * (i+1) - 1
+        great3.run(root, subfield_min=first, subfield_max=last,
+                   experiments=experiments, obs_type=obs_type, shear_type=shear_type,
+                   gal_dir=data_dir, ps_dir=ps_dir,
+                   seed=seed, steps=['config']
+                   )
+        for config_name in config_names:
+            name, ext = os.path.splitext(config_name)
+            new_name = '%s_%02d.yaml'%(name,i)
+            new_config_names.append(new_name)
+            shutil.copy(os.path.join(root,config_name),os.path.join(root,new_name))
+        for config_name in psf_config_names:
+            name, ext = os.path.splitext(config_name)
+            new_name = '%s_%02d_psf.yaml'%(name,i)
+            new_psf_config_names.append(new_name)
+            shutil.copy(os.path.join(root,config_name),os.path.join(root,new_name))
+    t2 = time.time()
+    print
+    print 'Time for great3.run config = ',t2-t1
+    print
 
 # build images using galsim_yaml
-t1 = time.time()
-os.chdir(root)
-for config_name in new_config_names:
-    t3 = time.time()
-    p = subprocess.Popen(['galsim_yaml',config_name,'-v1'])
-    p.communicate() # wait until done
-    t4 = time.time()
-    print 'Time for galsim_yaml',config_name,'= ',t4-t3
+    t1 = time.time()
+    os.chdir(root)
+    for config_name in new_config_names:
+        t3 = time.time()
+        p = subprocess.Popen(['galsim_yaml',config_name,'-v1'])
+        p.communicate() # wait until done
+        t4 = time.time()
+        print 'Time for galsim_yaml',config_name,'= ',t4-t3
+        print
+    for config_name in new_psf_config_names:
+        t3 = time.time()
+        p = subprocess.Popen(['galsim_yaml',config_name,'-v1'])
+        p.communicate() # wait until done
+        t4 = time.time()
+        print 'Time for galsim_yaml',config_name,'= ',t4-t3
+        print
+    os.chdir('..')
+    t2 = time.time()
+    print 'Total time for galsim_yaml = ',t2-t1
     print
-for config_name in new_psf_config_names:
-    t3 = time.time()
-    p = subprocess.Popen(['galsim_yaml',config_name,'-v1'])
-    p.communicate() # wait until done
-    t4 = time.time()
-    print 'Time for galsim_yaml',config_name,'= ',t4-t3
-    print
-os.chdir('..')
-t2 = time.time()
-print 'Total time for galsim_yaml = ',t2-t1
-print
 
 # Move these files to a different name, so we can compare them to the ones built in the next step.
-for ind in range(len(dirs)):
-    dir = dirs[ind]
-    n_epoch = n_epochs[ind]
-    for i in range(subfield_min, subfield_max+1):
-        for j in range(0, n_epoch):
-            f1 = os.path.join(dir,'image-%03d-%1d.fits'%(i,j))
-            f2 = os.path.join(dir,'yaml_image-%03d-%1d.fits'%(i,j))
-            shutil.move(f1,f2)
-            f1 = os.path.join(dir,'starfield_image-%03d-%1d.fits'%(i,j))
-            f2 = os.path.join(dir,'yaml_starfield_image-%03d-%1d.fits'%(i,j))
-            shutil.move(f1,f2)
+    for ind in range(len(dirs)):
+        dir = dirs[ind]
+        n_epoch = n_epochs[ind]
+        for i in range(subfield_min, subfield_max+1):
+            for j in range(0, n_epoch):
+                f1 = os.path.join(dir,'image-%03d-%1d.fits'%(i,j))
+                f2 = os.path.join(dir,'yaml_image-%03d-%1d.fits'%(i,j))
+                shutil.move(f1,f2)
+                f1 = os.path.join(dir,'starfield_image-%03d-%1d.fits'%(i,j))
+                f2 = os.path.join(dir,'yaml_starfield_image-%03d-%1d.fits'%(i,j))
+                shutil.move(f1,f2)
 
 # Build images using great3.run
 t1 = time.time()
@@ -150,23 +153,24 @@ print 'Time for great3.run images = ',t2-t1
 print
 
 # Check that images are the same.
-print 'Checking diffs: (No output means success)'
-sys.stdout.flush()
-for ind in range(len(dirs)):
-    dir = dirs[ind]
-    n_epoch = n_epochs[ind]
-    for i in range(subfield_min, subfield_max+1):
-        for j in range(0, n_epoch):
-            f1 = os.path.join(dir,'image-%03d-%1d.fits'%(i,j))
-            f2 = os.path.join(dir,'yaml_image-%03d-%1d.fits'%(i,j))
-            p = subprocess.Popen(['diff',f1,f2],stderr=subprocess.STDOUT)
-            p.communicate()
-            f1 = os.path.join(dir,'starfield_image-%03d-%1d.fits'%(i,j))
-            f2 = os.path.join(dir,'yaml_starfield_image-%03d-%1d.fits'%(i,j))
-            p = subprocess.Popen(['diff',f1,f2],stderr=subprocess.STDOUT)
-            p.communicate()
-print 'End diffs.'
-print
+if do_config:
+    print 'Checking diffs: (No output means success)'
+    sys.stdout.flush()
+    for ind in range(len(dirs)):
+        dir = dirs[ind]
+        n_epoch = n_epochs[ind]
+        for i in range(subfield_min, subfield_max+1):
+            for j in range(0, n_epoch):
+                f1 = os.path.join(dir,'image-%03d-%1d.fits'%(i,j))
+                f2 = os.path.join(dir,'yaml_image-%03d-%1d.fits'%(i,j))
+                p = subprocess.Popen(['diff',f1,f2],stderr=subprocess.STDOUT)
+                p.communicate()
+                f1 = os.path.join(dir,'starfield_image-%03d-%1d.fits'%(i,j))
+                f2 = os.path.join(dir,'yaml_starfield_image-%03d-%1d.fits'%(i,j))
+                p = subprocess.Popen(['diff',f1,f2],stderr=subprocess.STDOUT)
+                p.communicate()
+    print 'End diffs.'
+    print
 
 # Measure star parameters required for metric
 t1 = time.time()
