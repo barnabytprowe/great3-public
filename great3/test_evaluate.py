1e343c12ca80fa1009488438ba4645dcc277df51
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-09-07 23:43:27 -0700

    added testing for offsets to test_offset file (#28)

--- great3/test_evaluate.py
+++ great3/test_evaluate.py
@@ -22,7 +22,7 @@ grot = evaluate.get_generate_const_rotations(experiment, obs_type, logger=logger
 # Try a simple submission, no biases, and see what Q I get
 label = "sub1"
 g1sub, g2sub = g3metrics.make_submission_const_shear(
-   0.01, 0., -0.02, -0.02, g1t, g2t, 1e4, 0.05, label=label, rotate_cs=grot)
+   0.001, 0., -0.01, -0.01, g1t, g2t, 1e4, 0.05, label=label, rotate_cs=grot)
 subfile = "./g3subs/g3_const_shear_sub."+label+".dat"
 
 q, c1, m1, c2, m2, sigc1, sigm1, sigc2, sigm2  = evaluate.Q_const(subfile, 'control', 'ground')
@@ -33,6 +33,6 @@ print "m+ = "+str(m1)+" ("+str(sigm1)+")"
 print "cx = "+str(c2)+" ("+str(sigc2)+")"
 print "mx = "+str(m2)+" ("+str(sigm2)+")"
 
-
-
+subfield_index, offset_deg_x, offset_deg_y = evaluate.get_generate_variable_offsets(
+    experiment, obs_type, logger=logger)
 
