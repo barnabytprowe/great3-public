cbaf172042bb13ddb0f56cced3792c460010e883
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-09-11 17:53:14 -0700

    now just testing the variable stuff, constant stuff seems OK (#28)

--- great3/test_evaluate.py
+++ great3/test_evaluate.py
@@ -15,26 +15,26 @@ logger = logging.getLogger("test")
 logger.setLevel(logging.DEBUG)
 
 # Just try getting / building the intermediate products for this branch first
-sind, g1t, g2t = evaluate.get_generate_const_truth(experiment, obs_type, logger=logger)
-gdict = evaluate.get_generate_const_subfield_dict(experiment, obs_type, logger=logger)
-grot = evaluate.get_generate_const_rotations(experiment, obs_type, logger=logger)
+#sind, g1t, g2t = evaluate.get_generate_const_truth(experiment, obs_type, logger=logger)
+#gdict = evaluate.get_generate_const_subfield_dict(experiment, obs_type, logger=logger)
+#grot = evaluate.get_generate_const_rotations(experiment, obs_type, logger=logger)
 
 # Try a simple submission, no biases, and see what Q I get
-label = "sub1"
-g1sub, g2sub = g3metrics.make_submission_const_shear(
-   0.001, 0., 0., -0.01, g1t, g2t, 1e4, 0.05, label=label, rotate_cs=grot)
-subfile = "./g3subs/g3_const_shear_sub."+label+".dat"
+#label = "sub1"
+#g1sub, g2sub = g3metrics.make_submission_const_shear(
+#   0.001, 0., 0., -0.01, g1t, g2t, 1e4, 0.05, label=label, rotate_cs=grot)
+#subfile = "./g3subs/g3_const_shear_sub."+label+".dat"
 
-q, c1, m1, c2, m2, sigc1, sigm1, sigc2, sigm2  = evaluate.Q_const(subfile, 'control', 'ground')
+#q, c1, m1, c2, m2, sigc1, sigm1, sigc2, sigm2  = evaluate.Q_const(subfile, 'control', 'ground')
 
-print "Q_c = "+str(q)
-print "c+ = "+str(c1)+" ("+str(sigc1)+")"
-print "m+ = "+str(m1)+" ("+str(sigm1)+")"
-print "cx = "+str(c2)+" ("+str(sigc2)+")"
-print "mx = "+str(m2)+" ("+str(sigm2)+")"
+#print "Q_c = "+str(q)
+#print "c+ = "+str(c1)+" ("+str(sigc1)+")"
+#print "m+ = "+str(m1)+" ("+str(sigm1)+")"
+#print "cx = "+str(c2)+" ("+str(sigc2)+")"
+#print "mx = "+str(m2)+" ("+str(sigm2)+")"
 
-subfield_index, offset_deg_x, offset_deg_y = evaluate.get_generate_variable_offsets(
-    experiment, obs_type, logger=logger)
+#subfield_index, offset_deg_x, offset_deg_y = evaluate.get_generate_variable_offsets(
+#    experiment, obs_type, logger=logger)
 
 field, theta, map_E, map_B, maperr = evaluate.get_generate_variable_truth(
     experiment, obs_type, logger=logger)
