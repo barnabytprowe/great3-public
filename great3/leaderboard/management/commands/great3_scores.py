67c7bb9dcf99cecf1689f2e1ac83c79a54de1b5b
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-09-11 23:56:47 -0700

    made a first start on plumbing this into webpage (#28)

--- great3/leaderboard/management/commands/great3_scores.py
+++ great3/leaderboard/management/commands/great3_scores.py
@@ -1,13 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
 from leaderboard.models import Entry, PLACEHOLDER_SCORE, recompute_scoring
 from great3.settings import installation_base
+import evaluate
 import numpy as np
 import os
 import datetime
 import pytz
 import codecs
 
+EXPERIMENTS = (
+    "control",
+    "real_galaxy",
+    "variable_psf",
+    "multiepoch",
+    "full",
+    )
+
 SCORE_LOG_FILE = os.path.join(installation_base, "results", "results.log")
+
 
 class Command(BaseCommand):
 	args = ''
@@ -19,9 +29,21 @@ class Command(BaseCommand):
 		for entry in entries:
 			print "Processing entry %s" % entry.name.encode('utf-8')
 			filename = entry.get_filename()
+                        if entry.board.experiment is not in experiments:
+                            raise ValueError("Experiment not recognised.")
+                        if entry.board.space:
+                            obs_type = "space"
+                        else:
+                            obs_type = "ground"
+                        if entry.board.varying:
+                            shear_type = "variable"
+                        else:
+                            shear_type = "constant"
 			print "should compute score here, from loading entry filename ", filename
 			print "Instead using random number from 1-1000"
-			entry.score = np.random.uniform(1, 1000)
+			entry.score = evaluate.getq(
+                            filename, experiment, obs_type, shear_type, truth_dir="/great3/truth",
+                            storage_dir=STORAGE_DIR, logger=logger)
 			datestamp = datetime.datetime.utcnow().replace(tzinfo=pytz.utc).isoformat()
 			outfile.write('%s\t%s\t%r\t%s\n' % (entry.name,filename,entry.score,datestamp))
 			entry.save()
