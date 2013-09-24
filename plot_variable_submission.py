495335b7f6460310b532880d6cbca9e0c1942ce9
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-09-25 00:09:47 +0100

    added a plotting script for variable submissions, needs more work to say the least (#16)

---
+++ plot_variable_submission.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print "plot_variable_submission.py"
+        print "usage: ./plot_variable_submission.py input_submission output_filename"
+        sys.exit(1)
+
+    
