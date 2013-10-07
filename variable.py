fbf4369ead906fabb7d1f75ea5be3e78a58383cc
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-10-07 12:26:48 +0100

    added routines for making truth+intrinsic (#16)

--- variable.py
+++ variable.py
@@ -115,3 +115,4 @@ if __name__ == "__main__":
     idi, xi, yi, g1i, g2i = get_variable_gintrinsic("control", "space")
     ret = make_fits_cats(idt, g1t, g2t)
     ret = make_fits_cats(idi, g1i, g2i, prefix="gintrinsic")
+    ret = make_fits_cats(idi, g1i + g1t, g2i + g2t, prefix="gtrue_intrinsic")
