7f0832392ec61fc02c23a66c6185f6ebb433d43c
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-10-12 07:51:07 -0700

    modified constants.py in validation with updated pipeline & truth info (#16)

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-truth_dir = "/Users/browe/great3/truth"
+truth_dir = "/Users/browe/great3/truth-alpha-release-2"
 #public_dir = "/Users/browe/great3/great3-private/tests/test_run"
 public_dir = "/Users/browe/great3/public"
 
@@ -30,7 +30,7 @@ pipeline_columns = {
         "id": 0, "g1": 3, "g2": 4},
     "im3shape-1": { # First gen. alpha data
         "id": 0, "g1": 3, "g2": 4},
-    "hscpipe_shape_hsm_regauss-1a": { # First gen. alpha data with weight
+    "hscpipe_shape_hsm_regauss-2": { # Second gen. alpha data with weight
         "id": 0, "g1": 3, "g2": 4, "w": 5}, 
 }
 
