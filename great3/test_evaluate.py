f8d17590107bd00aae2289eb8e0585148c44b6df
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-10-14 17:25:42 -0700

    added intrinsic-shear getting code, testing (#28)

--- great3/test_evaluate.py
+++ great3/test_evaluate.py
@@ -69,8 +69,67 @@ def get_variable_gtrue(experiment, obs_type, logger=None):
             g1true[:, jsub, ifield] = truedata["g1"]
             g2true[:, jsub, ifield] = truedata["g2"]
             identifier[:, jsub, ifield] = truedata["ID"]
+
     # Then return
     return identifier, xx, yy, g1true, g2true
+
+def get_variable_gsuffix(experiment, obs_type, suffix="_intrinsic", file_prefix="galaxy_catalog",
+                         logger=None):
+    """Get the full catalog of intrinsic "shears" and positions for all fields.
+
+    Gets "g1"+suffix and "g2"+suffix from the subfield_catalog files.
+
+    @return id, x, y, g1, g2
+    """
+    mapper = great3sims.mapper.Mapper(evaluate.TRUTH_DIR, experiment, obs_type, "variable")
+    identifier = np.empty(
+        (evaluate.NGALS_PER_SUBFIELD, evaluate.NSUBFIELDS_PER_FIELD, evaluate.NFIELDS), dtype=int)
+    g1int = np.empty(
+        (evaluate.NGALS_PER_SUBFIELD, evaluate.NSUBFIELDS_PER_FIELD, evaluate.NFIELDS))
+    g2int = np.empty(
+        (evaluate.NGALS_PER_SUBFIELD, evaluate.NSUBFIELDS_PER_FIELD, evaluate.NFIELDS))
+    xx = np.empty(
+        (evaluate.NGALS_PER_SUBFIELD, evaluate.NSUBFIELDS_PER_FIELD, evaluate.NFIELDS))
+    yy = np.empty(
+        (evaluate.NGALS_PER_SUBFIELD, evaluate.NSUBFIELDS_PER_FIELD, evaluate.NFIELDS))
+    # Load the offsets
+    subfield_indices, offset_deg_x, offset_deg_y = evaluate.get_generate_variable_offsets(
+        experiment, obs_type, storage_dir=evaluate.STORAGE_DIR, truth_dir=evaluate.TRUTH_DIR,
+        logger=logger)
+    # Build basic x and y grids to use for coord positions
+    xgrid_deg, ygrid_deg = np.meshgrid(
+        np.arange(0., evaluate.XMAX_GRID_DEG, evaluate.DX_GRID_DEG),
+        np.arange(0., evaluate.XMAX_GRID_DEG, evaluate.DX_GRID_DEG))
+    xgrid_deg = xgrid_deg.flatten() # Flatten these - the default C ordering corresponds to the way
+    ygrid_deg = ygrid_deg.flatten() # the shears are ordered too, which is handy
+    if len(xgrid_deg) != evaluate.NGALS_PER_SUBFIELD:
+        raise ValueError(
+            "Dimensions of xgrid_deg and ygrid_deg do not match NGALS_PER_SUBFIELD.  Please check "+
+            "the values of XMAX_GRID_DEG and DX_GRID_DEG in evaluate.py.")
+    # Then loop over the fields and subfields getting the galaxy catalogues
+    import pyfits
+    for ifield in range(evaluate.NFIELDS):
+
+        # Read in all the shears in this field and store
+        for jsub in range(evaluate.NSUBFIELDS_PER_FIELD):
+
+            # Build the x,y grid using the subfield offsets
+            isubfield_index = jsub + ifield * evaluate.NSUBFIELDS_PER_FIELD
+            xx[:, jsub, ifield] = xgrid_deg + offset_deg_x[isubfield_index]
+            yy[:, jsub, ifield] = ygrid_deg + offset_deg_y[isubfield_index]
+            galcatfile = os.path.join(
+                mapper.full_dir, (file_prefix+"-%03d.fits" % isubfield_index))
+            truedata = pyfits.getdata(galcatfile)
+            if len(truedata) != evaluate.NGALS_PER_SUBFIELD:
+                raise ValueError(
+                    "Number of records in "+galcatfile+" (="+str(len(truedata))+") is not "+
+                    "equal to NGALS_PER_SUBFIELD (="+str(evaluate.NGALS_PER_SUBFIELD)+")")
+            g1int[:, jsub, ifield] = truedata["g1"+suffix]
+            g2int[:, jsub, ifield] = truedata["g2"+suffix]
+            identifier[:, jsub, ifield] = truedata["ID"]
+
+    # Then return
+    return identifier, xx, yy, g1int, g2int
 
 def make_variable_submission(x, y, g1true, g2true, g1int, g2int, c1, c2, m1, m2, outfile,
                              noise_sigma=0.05):
@@ -174,16 +233,14 @@ if __name__ == "__main__":
 
     # Then try making a fake submission ourselves
     _, x, y, g1true, g2true = get_variable_gtrue(experiment, obs_type)
-    result = make_variable_submission(x, y, g1true, g2true, 5.e-3, 2.e-4, 0.01, -0.01,
-        outfile="./g3subs/junk_map_test.dat")
-    q_biased = evaluate.q_variable("./g3subs/junk_map_test.dat", experiment, obs_type)
-    print "Q_v (from own biased submission simulator) = "+str(q_biased)
+    _, _, _, g1int, g2int = get_variable_suffix(experiment, obs_type)
 
     # Then perform a fiducial simualtion, and do up to NTEST trials, so as to help find an updated
     # normalization factor
     NTEST = 300
     result = make_variable_submission(
-        x, y, g1true, g2true, evaluate.CFID, evaluate.CFID, evaluate.MFID, evaluate.MFID,
+        x, y, g1true, g2true, g1int, g2int,
+        evaluate.CFID, evaluate.CFID, evaluate.MFID, evaluate.MFID,
         outfile="./g3subs/junk_map_test.dat")
     q = evaluate.q_variable("./g3subs/junk_map_test.dat", experiment, obs_type)
     print "Q_v (from own fiducial submission simulator) = "+str(q)
@@ -192,7 +249,8 @@ if __name__ == "__main__":
     for i in range(NTEST - 1):
     
         result = make_variable_submission(
-            x, y, g1true, g2true, evaluate.CFID, evaluate.CFID, evaluate.MFID, evaluate.MFID,
+            x, y, g1true, g2true, g1int, g2int,
+            evaluate.CFID, evaluate.CFID, evaluate.MFID, evaluate.MFID,
             outfile="./g3subs/junk_map_test.dat")
 
         q = evaluate.q_variable("./g3subs/junk_map_test.dat", experiment, obs_type)
