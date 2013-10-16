80e7248572f18770480efa54ea99b5a7a5e48cda
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-10-15 18:15:16 -0700

    added argparse to checksums.py (#16)

--- checksums.py
+++ checksums.py
@@ -60,6 +60,23 @@ def collate_all(root_dir, outfile, experiments=constants.experiments, obs_types=
     print "Full md5sum list written to "+str(outfile)
     return
 
+
 if __name__ == "__main__":
 
-    collate_all(constants.public_dir, "junk_md5sum.txt") 
+    description = \
+    """Get all the md5sums for the all the branches in the experiments, obs_types, shear_types
+    lists in validation/constants.py
+
+    Writes a full md5sum output file with each entry in the format <MD5 checksum> <filename> to
+    the specified outfile.
+    """
+    import argparse
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "outfile", type=str, help="Filename for ASCII file containing all md5sums calculated")
+    parser.add_argument(
+        "-root_dir", type=str, action="store_const", default=constant.public_dir,
+        help="Root directory for the GREAT3 release for which you want to calculate md5sums")
+    args = parser.parse_args()
+    collate_all(args.root_dir, args.outfile)
+
