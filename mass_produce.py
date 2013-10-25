21c3468f92388e6d4a8160541bef3bee492c596b
rmandelb <rmandelb@andrew.cmu.edu>
2013-10-25 15:57:43 -0400

    put back in the close_fds kwargs since they are not causing issues (#54)

--- mass_produce.py
+++ mass_produce.py
@@ -102,7 +102,7 @@ if not package_only:
         pbs_name = prefix1+e+o+s
         pbs_file = pbs_name+'.sh'
         command_str = 'qsub '+pbs_file
-        p = subprocess.Popen(command_str,shell=True)
+        p = subprocess.Popen(command_str,shell=True,close_fds=True)
     # The above command just submitted all the files to the queue.  We have to periodically poll the
     # queue to see if they are still running.
     mass_produce_utils.check_done('g3_step1', sleep_time=sleep_time)
@@ -123,7 +123,7 @@ if not package_only:
             command_str = 'qsub '+pbs_file
             if queue_nicely:
                 mass_produce_utils.check_njobs('g3_', sleep_time=sleep_time, n_jobs=queue_nicely)
-            p = subprocess.Popen(command_str, shell=True)
+            p = subprocess.Popen(command_str, shell=True, close_fds=True)
         mass_produce_utils.check_done('g3_', sleep_time=sleep_time)
         t2 = time.time()
         # Times are approximate since check_done only checks every N seconds for some N
@@ -150,7 +150,7 @@ for experiment, obs_type, shear_type in branches:
                                      n_config_per_branch, preload, my_step=3)
     # And then submit them
     command_str = 'qsub '+pbs_file
-    p = subprocess.Popen(command_str,shell=True)
+    p = subprocess.Popen(command_str,shell=True, close_fds=True)
 # The above command just submitted all the files to the queue.  We have to periodically poll the
 # queue to see if they are still running.
 mass_produce_utils.check_done('g3_step3', sleep_time=sleep_time)
