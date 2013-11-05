3e05593702cd04fad59026e5cedc4037436cf011
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-11-05 11:41:27 -0800

    added license statement to presubmission.py, with tiny mods

--- presubmission_script/presubmission.py
+++ presubmission_script/presubmission.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
-""" 
-presubmission.py: a script to generate summary statistics for the Great3 challenge 
+
+#    presubmission.py: a script to generate summary statistics for submission
+#    to the GREAT3 Challenge (http://great3challenge.info)
+#    Copyright (C) Melanie Simet & the GREAT3 Team:
+#    https://github.com/barnabytprowe/great3-public
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+presubmission.py: a script to generate summary statistics for submission to the GREAT3 challenge
 (http://great3challenge.info).
 
 General information for the use of this script can be found in the README, which should have been
