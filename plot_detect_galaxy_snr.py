dfccbde71fa1013d7e5504b954dbe517d2e2b14b
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-12-09 22:14:35 +0000

    changed histtype to step for multiple plotting (#16)

--- plot_detect_galaxy_snr.py
+++ plot_detect_galaxy_snr.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     snr1 = data1["FLUX_AUTO"] / data1["FLUXERR_AUTO"]
     infilesplit1 = infile1.split("/")
     label1 = infilesplit1[-4]+"/"+infilesplit1[-3]+"/"+infilesplit1[-2]+"/"+infilesplit1[-1]
-    plt.hist(snr1, bins=100, range=(0, 100), label=label1)
+    plt.hist(snr1, bins=100, range=(0, 100), label=label1, histtype="step")
     if len(sys.argv) == 4:
         infile2 = sys.argv[2]
         outfile = sys.argv[3]
@@ -26,7 +26,7 @@ if __name__ == "__main__":
         snr2 = data2["FLUX_AUTO"] / data2["FLUXERR_AUTO"]
         infilesplit2 = infile2.split("/")
         label2 = infilesplit2[-4]+"/"+infilesplit2[-3]+"/"+infilesplit2[-2]+"/"+infilesplit2[-1]
-        plt.hist(snr2, bins=100, range=(0, 100), label=label2)
+        plt.hist(snr2, bins=100, range=(0, 100), label=label2, histtype="step")
     else:
         outfile = sys.argv[2]
     plt.legend()
