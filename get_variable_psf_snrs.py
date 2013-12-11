98e9e787cc397b9d65e2078f3bee6c00d2b47454
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-12-11 15:30:38 +0000

    revamp, trying again after stupid fail (#63)

--- get_variable_psf_snrs.py
+++ get_variable_psf_snrs.py
@@ -10,45 +10,48 @@ import great3sims.mapper
 EXPERIMENT = "variable_psf"
 
 
-def make_cats_dict(experiment):
+def make_cats(experiment, obs_type, shear_type, catfile_prefix="./counts/sexcat_"):
     """Make a dict of SExtractor catalogs for all galaxy images in all branches of this experiment
     """
-    print "Beginning generation of catalogs to be stored into "+catfile
-
-    # Setup storage dictionary
+    mapper = great3sims.mapper.Mapper(
+        constants.public_dir, experiment, obs_type, shear_type)
+    print "Making catalogs for galaxy images in "+mapper.full_dir
     cats = {}
-    for obs_type in constants.obs_types:
+    for subfield_index in range(200):
 
-        for shear_type in constants.shear_types:
-
-            mapper = great3sims.mapper.Mapper(
-                constants.public_dir, experiment, obs_type, shear_type)
-            for subfield_index in range(200):
-
-                imfile = os.path.join(mapper.full_dir, "image-%03d-0.fits" % subfield_index)
-                print "Running SExtractor on "+imfile
-                if obs_type == "space":
-                    config_filename = "sex_space.config"
-                else:
-                    config_filename = "sex.config"
-                cats[experiment+"-"+obs_type+"-"+shear_type+"/"+(os.path.split(imfile)[-1])] = \
-                    count_objects.file_count(imfile, config_filename=config_filename)
-    # Return the filled dict
+        imfile = os.path.join(mapper.full_dir, "image-%03d-0.fits" % subfield_index)
+        catfile = catfile_prefix+experiment+"-"+obs_type+"-"+shear_type+\
+            ("_image-%03d-0" % subfield_index)+".fits"
+        if not os.path.isfile(catfile):
+            print "Running SExtractor on "+imfile+" to build "+catfile
+            if obs_type == "space":
+                config_filename = "sex_space.config"
+            else:
+                config_filename = "sex.config"
+            subprocess.check_call(
+                ["sex", imfile, "-c", config_filename, "-CATALOG_NAME", catfile], close_fds=True)
+        print "Loading data from "+catfile
+        cats[imfile] = pyfits.getdata(catfile)
+    # Return the cats dict
     return cats
-
 
 if __name__ == "__main__":
 
     import cPickle
     # First of all make the counts output directory and assign the filename for catalog output
     if not os.path.isdir("./counts"): os.mkdir("./counts")
-    catfile = os.path.join(".counts", "variable_psf_sexcats.p")
-    if not os.path.isfile(catfile):
-        cats = make_cats_dict(EXPERIMENT)
-        print "Saving catalogs in "+catfile
-        with open(catfile, "wb") as fout:
-            cPickle.dump(cats, fout)
-    else:
-        with open(catfile, "rb") as fin:
-            cats = cPickle.load(fin)
+    for obs_type in constants.obs_types:
+
+        for shear_type in constants.shear_types:
+
+            cats = make_cats(EXPERIMENT, obs_type, shear_type)
+            for imfile, cat in cats.iteritems():
+
+                snr = cat["FLUX_AUTO"] / cat["FLUXERR_AUTO"]
+                plt.hist(snr, bins=100, range=(0, 100), histtype="step")
+                plt.xlabel("FLUX_AUTO / FLUXERR_AUTO")
+                plt.ylabel("Counts")
+                plt.show()
+                1/0
+
 
