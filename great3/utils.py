ea96531828b6fff526437858329a0b0d3e5247d5
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-12-30 14:44:32 +0000

    updated utils to be calling location independent (portable) (#67)

--- great3/utils.py
+++ great3/utils.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import numpy as np
-sys.path.append(os.path.join("..", "..", "validation"))
+path, module = os.path.split(__file__)
+sys.path.append(os.path.join(path, "..", "..", "validation"))
 import constants
 import evaluate
 
