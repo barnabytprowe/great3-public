de2a651e80b505d695c986e393b8009ac01ac3cc
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2014-01-31 16:25:47 +0000

    first pass at improving the estimates of correlated results (#67)

--- tabulate_correlated_const_shear_metric_rev1.py
+++ tabulate_correlated_const_shear_metric_rev1.py
@@ -33,132 +33,175 @@ MVALS = evaluate.MFID * 10.**(.5 * np.arange(5))
 # im3shape, see calculate_im3shape_regauss_correlation.py)
 RHO = 0.6
 
+NRUNS = 30 # Number of runs to do from which to estimate the average std(Q_c), see comment below.
+
+# Filenames for final mean standard deviation results averaged over NRUNS runs:
+CSTDOUTFILE = os.path.join(
+    "results","tabulated_correlated_std_const_Q_c_versus_c_NRUNS"+str(NRUNS)+".pkl")
+MSTDOUTFILE = os.path.join(
+    "results","tabulated_correlated_std_const_Q_c_versus_m_NRUNS"+str(NRUNS)+".pkl")
+
 if __name__ == "__main__":
 
     import cPickle
-    # Dicts containing arrays for storing Q_c values versus m and c, for ground and space
-    qc = {"ground": np.empty((NTEST, len(CVALS))), "space": np.empty((NTEST, len(CVALS)))}
-    qm = {"ground": np.empty((NTEST, len(MVALS))), "space": np.empty((NTEST, len(MVALS)))}
-    coutfile = os.path.join("results", "tabulated_correlated_const_Q_c_versus_c_norm2.pkl")
-    moutfile = os.path.join("results", "tabulated_correlated_const_Q_c_versus_m_norm2.pkl")
+    # Note I've updated this code because I noticed that the standard deviation of correlated Q_c
+    # values, which is the main thing of interest that gets calculated by this script, is a strong
+    # function of what the *mean* Q_c is per bin.  As these are all correlated, this changes quite
+    # a bit from run to run!  Therefore I am going to repeat the calculations below NRUN times to
+    # sample from a range of these results and take the average std(Q_c) to get something more
+    # representative
+    qclist = []
+    qmlist = []
+    for krun in range(NRUNS):
 
-    if not os.path.isfile(coutfile):
-        for obs_type in ("ground", "space",):
+        # Dicts containing arrays for storing Q_c values versus m and c, for ground and space
+        qc = {"ground": np.empty((NTEST, len(CVALS))), "space": np.empty((NTEST, len(CVALS)))}
+        qm = {"ground": np.empty((NTEST, len(MVALS))), "space": np.empty((NTEST, len(MVALS)))}
+        coutfile = os.path.join(
+            "results", "tabulated_correlated_const_Q_c_versus_c_norm"+str(krun)+".pkl")
+        moutfile = os.path.join(
+            "results", "tabulated_correlated_const_Q_c_versus_m_norm"+str(krun)+".pkl")
+        if not os.path.isfile(coutfile):
+            for obs_type in ("ground", "space",):
 
-            print "Calculating Q_c values versus c for control-"+obs_type+"-constant data in GREAT3"
-            print "NOISE_SIGMA = "+str(NOISE_SIGMA[obs_type])
-            print "RHO = "+str(RHO)
-            # First we build the truth table
-            print "Building truth tables for control-"+obs_type+"-constant"
-            subfield_index, g1true, g2true = evaluate.get_generate_const_truth(
-                EXPERIMENT, obs_type, truth_dir=TRUTH_DIR)
-            rotations = evaluate.get_generate_const_rotations(
-                EXPERIMENT, obs_type, truth_dir=TRUTH_DIR)
-            for jc, cval in enumerate(CVALS):
+                print "Calculating Q_c values versus c for control-"+obs_type+\
+                    "-constant data in GREAT3"
+                print "NOISE_SIGMA = "+str(NOISE_SIGMA[obs_type])
+                print "RHO = "+str(RHO)
+                # First we build the truth table
+                print "Building truth tables for control-"+obs_type+"-constant"
+                subfield_index, g1true, g2true = evaluate.get_generate_const_truth(
+                    EXPERIMENT, obs_type, truth_dir=TRUTH_DIR)
+                rotations = evaluate.get_generate_const_rotations(
+                    EXPERIMENT, obs_type, truth_dir=TRUTH_DIR)
+                for jc, cval in enumerate(CVALS):
 
-                # Build the submissions
-                g1sub, g2sub = g3metrics.make_multiple_submissions_const_shear(
+                    # Build the submissions
+                    g1sub, g2sub = g3metrics.make_multiple_submissions_const_shear(
                         cval, 0., evaluate.MFID, evaluate.MFID, g1true, g2true, NGALS_PER_IMAGE,
                         NOISE_SIGMA[obs_type], rotate_cs=rotations, nsubmissions=NTEST, rho=RHO)
-                # Loop over submissions evaluating metric
-                for itest in xrange(NTEST):
+                    # Loop over submissions evaluating metric
+                    for itest in xrange(NTEST):
 
-                    fdsub, subfile = tempfile.mkstemp(suffix=".dat")
-                    with os.fdopen(fdsub, "wb") as fsub:
-                        np.savetxt(
-                            fsub, np.array((subfield_index, g1sub[:, itest], g2sub[:, itest])).T,
-                            fmt="%d %e %e")
-                    qc[obs_type][itest, jc] = evaluate.q_constant(
-                        subfile, EXPERIMENT, obs_type, just_q=True, truth_dir=TRUTH_DIR)
-                    os.remove(subfile)
-                    #print "Test %4d / %4d (c+ = %.3e) Q_c = %.4f" % (
-                    #    itest+1, NTEST, cval, qc[obs_type][itest, jc])
+                        fdsub, subfile = tempfile.mkstemp(suffix=".dat")
+                        with os.fdopen(fdsub, "wb") as fsub:
+                            np.savetxt(
+                                fsub, np.array((subfield_index, g1sub[:, itest],
+                                g2sub[:, itest])).T, fmt="%d %e %e")
+                        qc[obs_type][itest, jc] = evaluate.q_constant(
+                            subfile, EXPERIMENT, obs_type, just_q=True, truth_dir=TRUTH_DIR)
+                        os.remove(subfile)
 
-                print "std(Q_c) = "+str(qc[obs_type][:, jc].std())+" for "+str(NTEST)+\
-                        " sims (with c+ = "+str(cval)+", obs_type = "+str(obs_type)+")"
-                print
+                    print "mean(Q_c), std(Q_c) = "+str(qc[obs_type][:, jc].mean())+", "+\
+                        str(qc[obs_type][:, jc].std())+" for "+str(NTEST)+" sims (with c+ = "+\
+                        str(cval)+", obs_type = "+str(obs_type)+")"
+                    print
 
-        print "Saving pickled Q_c versus c dict to "+coutfile
-        with open(coutfile, "wb") as fout:
-            cPickle.dump(qc, fout)
+            print "Saving pickled Q_c versus c dict to "+coutfile
+            with open(coutfile, "wb") as fout: cPickle.dump(qc, fout)
+            print
+        else:
+            with open(coutfile, "rb") as fin: qc = cPickle.load(fin)
+        if not os.path.isfile(moutfile):
+            for obs_type in ("ground", "space",):
+
+                print "Calculating Q_c values versus m for control-"+obs_type+\
+                    "-constant data in GREAT3"
+                print "NOISE_SIGMA = "+str(NOISE_SIGMA[obs_type])
+                print "RHO = "+str(RHO)
+                # First we build the truth table
+                print "Building truth tables for control-"+obs_type+"-constant"
+                subfield_index, g1true, g2true = evaluate.get_generate_const_truth(
+                    EXPERIMENT, obs_type, truth_dir=TRUTH_DIR)
+                rotations = evaluate.get_generate_const_rotations(
+                    EXPERIMENT, obs_type, truth_dir=TRUTH_DIR)
+                for jm, mval in enumerate(MVALS):
+
+                    # Build the submissions
+                    g1sub, g2sub = g3metrics.make_multiple_submissions_const_shear(
+                        evaluate.CFID, 0., mval, mval, g1true, g2true, NGALS_PER_IMAGE,
+                        NOISE_SIGMA[obs_type], rotate_cs=rotations, nsubmissions=NTEST, rho=RHO)
+                    # Loop over submissions evaluating the metric
+                    for itest in xrange(NTEST):
+
+                        fdsub, subfile = tempfile.mkstemp(suffix=".dat")
+                        with os.fdopen(fdsub, "wb") as fsub:
+                            np.savetxt(
+                                fsub, np.array((subfield_index, g1sub[:, itest],
+                                g2sub[:, itest],)).T, fmt="%d %e %e")
+                        qm[obs_type][itest, jm] = evaluate.q_constant(
+                            subfile, EXPERIMENT, obs_type, just_q=True, truth_dir=TRUTH_DIR)
+                        os.remove(subfile)
+
+                    print "mean(Q_c), std(Q_c) = "+str(qm[obs_type][:, jm].mean())+", "+\
+                        str(qm[obs_type][:, jm].std())+" for "+str(NTEST)+" sims (with m = "+\
+                        str(mval)+", obs_type = "+str(obs_type)+")"
+                    print
+
+            print "Saving pickled Q_c versus m dict to "+moutfile
+            with open(moutfile, "wb") as fout: cPickle.dump(qm, fout)
+            print
+        else:
+            with open(moutfile, "rb") as fin: qm = cPickle.load(fin)
         print
-    else:
-        with open(coutfile, "rb") as fin:
-            qc = cPickle.load(fin)
+        print "Table of Q_c (ground sims) at constant m = mfid = "+str(evaluate.MFID)
+        print "    c+       Q_c    std(Q_c)"
+        for c, Q, dQ in zip(CVALS, np.mean(qc["ground"], axis=0), np.std(qc["ground"], axis=0)):
 
-    if not os.path.isfile(moutfile):
-        for obs_type in ("ground", "space",):
+            print "{:8f} {:8.3f} {:8.3f}".format(c, Q, dQ)
 
-            print "Calculating Q_c values versus m for control-"+obs_type+"-constant data in GREAT3"
-            print "NOISE_SIGMA = "+str(NOISE_SIGMA[obs_type])
-            print "RHO = "+str(RHO)
-            # First we build the truth table
-            print "Building truth tables for control-"+obs_type+"-constant"
-            subfield_index, g1true, g2true = evaluate.get_generate_const_truth(
-                EXPERIMENT, obs_type, truth_dir=TRUTH_DIR)
-            rotations = evaluate.get_generate_const_rotations(
-                EXPERIMENT, obs_type, truth_dir=TRUTH_DIR)
-            for jm, mval in enumerate(MVALS):
-
-                # Build the submissions
-                g1sub, g2sub = g3metrics.make_multiple_submissions_const_shear(
-                    evaluate.CFID, 0., mval, mval, g1true, g2true, NGALS_PER_IMAGE,
-                    NOISE_SIGMA[obs_type], rotate_cs=rotations, nsubmissions=NTEST, rho=RHO)
-                # Loop over submissions evaluating the metric
-                for itest in xrange(NTEST):
-
-                    fdsub, subfile = tempfile.mkstemp(suffix=".dat")
-                    with os.fdopen(fdsub, "wb") as fsub:
-                        np.savetxt(
-                            fsub, np.array((subfield_index, g1sub[:, itest], g2sub[:, itest],)).T,
-                            fmt="%d %e %e")
-                    qm[obs_type][itest, jm] = evaluate.q_constant(
-                        subfile, EXPERIMENT, obs_type, just_q=True, truth_dir=TRUTH_DIR)
-                    os.remove(subfile)
-                    #print "Test %4d / %4d (m = %.3e) Q_c = %.4f" % (
-                    #    itest+1, NTEST, mval, qm[obs_type][itest, jm])
-
-                print "std(Q_c) = "+str(qm[obs_type][:, jm].std())+" for "+str(NTEST)+\
-                    " sims (with m = "+str(mval)+", obs_type = "+str(obs_type)+")"
-                print
-
-        print "Saving pickled Q_c versus m dict to "+moutfile
-        with open(moutfile, "wb") as fout:
-            cPickle.dump(qm, fout)
         print
-    else:
-        with open(moutfile, "rb") as fin:
-            qm = cPickle.load(fin)
+        print "Table of Q_c (space sims) at constant m = mfid = "+str(evaluate.MFID)
+        print "    c+       Q_c    std(Q_c)"
+        for c, Q, dQ in zip(CVALS, np.mean(qc["space"], axis=0), np.std(qc["space"], axis=0)):
 
-    print ""
-    print "Table of Q_c (ground sims) at constant m = mfid = "+str(evaluate.MFID)
-    print "    c+       Q_c    std(Q_c)"
-    for c, Q in zip(CVALS, np.mean(qc["ground"], axis=0), np.std(qc["ground"], axis=0)):
+            print "{:8f} {:8.3f} {:8.3f}".format(c, Q, dQ)
 
-        print "{:8f} {:8.3f}".format(c, Q)
+        print
+        print "Table of Q_c (ground sims) at constant c = cfid = "+str(evaluate.CFID)
+        print "    m        Q_c    std(Q_c)"
+        for m, Q, dQ in zip(MVALS, np.mean(qm["ground"], axis=0), np.std(qm["ground"], axis=0)):
 
-    print ""
-    print "Table of Q_c (space sims) at constant m = mfid = "+str(evaluate.MFID)
-    print "    c+       Q_c    std(Q_c)"
-    for c, Q in zip(CVALS, np.mean(qc["space"], axis=0), np.std(qc["space"], axis=0)):
+            print "{:8f} {:8.3f} {:8.3f}".format(m, Q, dQ)
 
-        print "{:8f} {:8.3f}".format(c, Q)
+        print
+        print "Table of Q_c (space sims) at constant c = cfid = "+str(evaluate.CFID)
+        print "    m        Q_c    std(Q_c)"
+        for m, Q, dQ in zip(MVALS, np.mean(qm["space"], axis=0), np.std(qm["space"], axis=0)):
 
-    print ""
-    print "Table of Q_c (ground sims) at constant c = cfid = "+str(evaluate.CFID)
-    print "    m        Q_c    std(Q_c)"
-    for m, Q in zip(MVALS, np.mean(qm["ground"], axis=0), np.std(qm["ground"], axis=0)):
+            print "{:8f} {:8.3f} {:8.3f}".format(m, Q, dQ)
 
-        print "{:8f} {:8.3f}".format(m, Q)
+        qclist.append(qc)
+        qmlist.append(qm)
 
-    print ""
-    print "Table of Q_c (space sims) at constant c = cfid = "+str(evaluate.CFID)
-    print "    m        Q_c    std(Q_c)"
-    for m, Q in zip(MVALS, np.mean(qm["space"], axis=0), np.std(qm["space"], axis=0)):
+    # Then tabulate the average statistics across these NRUNS runs
+    qcmean = {"ground": np.empty(len(CVALS)), "space": np.empty(len(CVALS))}
+    sqcmean = {"ground": np.empty(len(CVALS)), "space": np.empty(len(CVALS))}
+    qmmean = {"ground": np.empty(len(MVALS)), "space": np.empty(len(MVALS))}
+    sqmmean = {"ground": np.empty(len(MVALS)), "space": np.empty(len(MVALS))}
+    for obs_type in ("ground", "space"):
 
-        print "{:8f} {:8.3f}".format(m, Q)
+        for qc, qm in zip(qclist, qmlist):
 
-    
+            qcmean[obs_type] += np.mean(qc[obs_type], axis=0) / float(NRUNS)
+            qmmean[obs_type] += np.mean(qm[obs_type], axis=0) / float(NRUNS)
+            sqcmean[obs_type] += np.std(qc[obs_type], axis=0) / float(NRUNS)
+            sqmmean[obs_type] += np.std(qm[obs_type], axis=0) / float(NRUNS)
 
+        print
+        print
+        print "OVERALL table of average Q_c and std(Q_c) from NRUNS = "+str(NRUNS)+" tests"
+        print "At constant m1 = m2 = mfid = "+str(evaluate.MFID)
+        print "    c        Q_c    std(Q_c)"
+        for c, Q, dQ in zip(CVALS, qcmean[obs_type], sqcmean[obs_type]):
 
+            print "{:8f} {:8.3f} {:8.3f}".format(c, Q, dQ)
 
+        print "At constant c1 = cfid = "+str(evaluate.CFID)
+        print "    m        Q_c    std(Q_c)"
+        for c, Q, dQ in zip(CVALS, qcmean[obs_type], sqcmean[obs_type]):
+
+            print "{:8f} {:8.3f} {:8.3f}".format(m, Q, dQ)
+
+    with open(CSTDOUTFILE, "wb") as fout: cPickle.dump(sqcmean, fout)
+    with open(MSTDOUTFILE, "wb") as fout: cPickle.dump(sqmmean, fout)
