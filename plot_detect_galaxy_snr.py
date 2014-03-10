9ee9e2192d62e5fb1c0d9ada5356fd796c56bc4d
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2014-03-10 23:23:47 +0000

    commented out bits from checking for Rachel (#63)

--- plot_detect_galaxy_snr.py
+++ plot_detect_galaxy_snr.py
@@ -18,11 +18,10 @@ if __name__ == "__main__":
     snr1 = data1["FLUX_AUTO"] / data1["FLUXERR_AUTO"]
     infilesplit1 = infile1.split("/")
     label1 = infilesplit1[-4]+"/"+infilesplit1[-3]+"/"+infilesplit1[-2]+"/"+infilesplit1[-1]
-    plt.hist(snr1, bins=100, range=(0, 1000), label=label1, histtype="step")
-    import itertools
-    for x, y, snr in itertools.izip(data1['X_IMAGE'], data1['Y_IMAGE'], snr1):
-        if snr > 100.: print "%.4f %.4f %.4f" % (x, y, snr)
-
+    plt.hist(snr1, bins=100, range=(0, 100), label=label1, histtype="step")
+    #import itertools
+    #for x, y, snr in itertools.izip(data1['X_IMAGE'], data1['Y_IMAGE'], snr1):
+    #    if snr > 100.: print "%.4f %.4f %.4f" % (x, y, snr)
     if len(sys.argv) == 4:
         infile2 = sys.argv[2]
         outfile = sys.argv[3]
