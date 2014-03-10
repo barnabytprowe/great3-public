12f011f460eae32ec98441d0842802e58507ddcb
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2014-03-10 22:21:13 +0000

    some hacky edits to plot_detect_snr (#63)

--- plot_detect_galaxy_snr.py
+++ plot_detect_galaxy_snr.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     snr1 = data1["FLUX_AUTO"] / data1["FLUXERR_AUTO"]
     infilesplit1 = infile1.split("/")
     label1 = infilesplit1[-4]+"/"+infilesplit1[-3]+"/"+infilesplit1[-2]+"/"+infilesplit1[-1]
-    plt.hist(snr1, bins=100, range=(0, 100), label=label1, histtype="step")
+    plt.hist(snr1, bins=100, range=(0, 1000), label=label1, histtype="step")
+    print np.max(snr1)
     if len(sys.argv) == 4:
         infile2 = sys.argv[2]
         outfile = sys.argv[3]
