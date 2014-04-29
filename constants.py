368abca16f26ff0698b1db64cea9bd5a81d95721
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2014-04-28 22:31:58 -0400

    better commenting in constants.py (#77)

--- constants.py
+++ constants.py
@@ -23,14 +23,16 @@
 # IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-Global constants, some of which are dictionaries that depend on the experiment, observation, or
-shear type.
+Global constants for the GREAT3 simulations, some of which are dictionaries that depend on the
+experiment, observation, or shear type.
 """
 import numpy
 
-image_size_deg = 10. # linear extent of one of our images.
+# Linear extent of one of our images, in degrees.
+image_size_deg = 10.
 
-nrows = 100   # Number of rows, columns of galaxies per subfield
+# Number of rows, columns of galaxies per subfield.
+nrows = 100
 ncols = 100
 
 # Define the size of each galaxy postage stamp in pixels.  We want to enable different values
@@ -55,26 +57,27 @@ xsize = {
 # Let's just require the PS to always be square.
 ysize = xsize
 
-xcells = 15   # When placing stars for variable_psf images, we split the image into cells and assign
-ycells = 15   # one star per cell.  This avoids blending and rare pathological clustering.
-
-# When making star fields for constant PSF images, we make a grid of PSFs.  One of them is perfectly
-# centered, the others have random offsets that are unknown to the participants, in the range
-# +/-centroid_shift_max.
+# When making star fields for constant PSF images, we make a grid of nx_constpsf x ny_constpsf PSFs.
+# One of them is perfectly centered; the others have random offsets that are unknown to the
+# participants, in the range +/-centroid_shift_max pixels.
 nx_constpsf = 3
 ny_constpsf = 3
 
-n_subfields = 220   # Number of galaxy/shear catalogs per experiment.  This must be an integer
-# multiple of n_subfields_per_field, and should include the number of deep subfields (for which more
-# parameters are defined below).
+# Number of galaxy/shear catalogs per experiment.  This must be an integer multiple of
+# n_subfields_per_field, and should include the number of deep subfields (for which more parameters
+# are defined below).
+n_subfields = 220
 
-centroid_shift_max = 1.0  # Shift galaxy centroids by uniform random in [-, +] in PIXELS.
+# Shift galaxy centroids by uniform random in [-, +] in PIXELS.
+centroid_shift_max = 1.0
 
-epoch_shift_max = 1.0    # Shift epochs relative to the subfield by uniform random in [-, +]
-                         # in PIXELS.
+# Shift epochs relative to the subfield by uniform random in [-, +] in PIXELS.
+epoch_shift_max = 1.0
 
-n_epochs = 6  # Number of epochs for each subfield when multiepoch
+# Number of epochs for each subfield when multiepoch
+n_epochs = 6
 
+# Pixel scale, in arcsec.
 pixel_scale = {
     "space": { # index second level of dictionary based on bool multiepoch
         True: 0.10, # not Nyquist sampled when there are multiple epochs
@@ -94,7 +97,6 @@ pixel_scale = {
 # type, and then based on the boolean variable_psf, which is True for the variable_psf and full experiments
 # and False for the others.  The idea is that branches with variable *anything* should have 20
 # subfields per field, and branches with constant shear AND PSF should have 1 subfield per field.
-
 n_subfields_per_field = {
     # first layer of indexing based on shear type
     "variable": { # second layer of indexing based on bool variable_psf
@@ -112,18 +114,19 @@ n_subfields_per_field = {
 subfield_grid_subsampling = 7
 
 # Parameters related to the "deep" subfields, which are always generated at the end of the branch,
-# after all the regular subfields.
-n_deep_subfields = 20 # Number of deep subfields to generate per branch (must be multiple of
-                      # n_subfields_per_field).
-deep_frac = 0.025     # Relative size of the deep dataset compared to the regular one, when it comes
-                      # to what is actually distributed with the challenge.  It must be the case
-                      # that deep_frac <= n_deep_subfields / (n_subfields - n_deep_subfields).
-deep_variance_mult = 0.0625 # Factor by which the noise variance should be reduced compared to that
-                            # in the regular fields when creating the deep fields.  This comes from
-                            # the idea that the depth is supposed to differ by ~1 mag, which would
-                            # mean increasing fluxes by 4 or keeping fluxes fixed while lowering the
-                            # noise by 1/4 (or noise variance by 1/4^2).
+# after all the regular subfields:
+# Number of deep subfields to generate per branch (must be integer multiple of n_subfields_per_field).
+n_deep_subfields = 20
+# Relative size of the deep dataset compared to the regular one, when it comes to what is actually
+# distributed with the challenge.  It must be the case that deep_frac <= n_deep_subfields /
+# (n_subfields - n_deep_subfields).
+deep_frac = 0.025
+# Factor by which the noise variance should be reduced compared to that in the regular fields when
+# creating the deep fields.  This comes from the idea that the depth is supposed to differ by ~1
+# mag, which would mean increasing fluxes by 4 or keeping fluxes fixed while lowering the noise by
+# 1/4 (or noise variance by 1/4^2).
+deep_variance_mult = 0.0625
 
-# For variable PSF case, set a stellar density and number of stars per image
+# For the variable PSF case, set a stellar density and number of stars per image.
 min_star_density = 1. # per arcmin^2
 max_star_density = 3.
