0889d31d8c656fd22b7683d2711943ccd1dd18cb
Joe Zuntz <joezuntz@googlemail.com>
2014-05-05 10:05:37 +0100

    add script to update DB cols (#79)

---
+++ data/add-db-cols.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+con = sqlite3.connect('great3.sqlite3')
+c = con.cursor()
+c.execute("alter table leaderboard_board add column 'frozen' 'bool' NOT NULL default True")
+c.execute("alter table leaderboard_board add column 'blind' 'bool' NOT NULL  default True")
+
+c.execute("alter table leaderboard_entry add column 'm' 'float' NOT NULL default 1.0")
+c.execute("alter table leaderboard_entry add column 'c' 'float' NOT NULL default 1.0")
+c.execute("alter table leaderboard_entry add column 'delta_m' 'float' NOT NULL default 1.0")
+c.execute("alter table leaderboard_entry add column 'delta_c' 'float' NOT NULL default 1.0")
+c.execute("alter table leaderboard_entry add column 'blind' 'bool' NOT NULL default True")
