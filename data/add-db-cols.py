31f63b1dfa72edf0de85eef4a855361bc2f4e72a
Joe Zuntz <joezuntz@googlemail.com>
2014-05-05 10:39:05 +0100

    add imported bool to db script and change default (#79)

--- data/add-db-cols.py
+++ data/add-db-cols.py
@@ -9,4 +9,5 @@ c.execute("alter table leaderboard_entry add column 'm' 'float' NOT NULL default
 c.execute("alter table leaderboard_entry add column 'c' 'float' NOT NULL default 1.0")
 c.execute("alter table leaderboard_entry add column 'delta_m' 'float' NOT NULL default 1.0")
 c.execute("alter table leaderboard_entry add column 'delta_c' 'float' NOT NULL default 1.0")
-c.execute("alter table leaderboard_entry add column 'blind' 'bool' NOT NULL default 1")
+c.execute("alter table leaderboard_entry add column 'blind' 'bool' NOT NULL default 0")
+c.execute("alter table leaderboard_entry add column 'imported' 'bool' NOT NULL default 0")
