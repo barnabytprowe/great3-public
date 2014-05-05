b869b5a7f5191eb7661aa455a7e6fc3b92e53b5d
Joe Zuntz <joezuntz@googlemail.com>
2014-05-05 15:33:03 +0100

    update post-challenge leaderboard page and template tag helper (#79)

---
+++ great3/leaderboard/templatetags/g3_template_tags.py
@@ -0,0 +1,24 @@
+#coding: utf-8
+from django import template
+from leaderboard.models import PLACEHOLDER_ERROR
+
+register = template.Library()
+
+@register.filter
+def with_error(value, arg):
+	if float(value)==PLACEHOLDER_ERROR:
+		return ""
+	return "%.3g ± %.3g" % (value, arg)
+
+@register.filter
+def split_lines(value):
+	value = str(value)
+	if len(value)>16:
+		value = value[:16] + "\n" + value[16:]
+		print value
+	return value
+
+
+@register.filter
+def shorten_name(value):
+	return str(value).replace("_", " ")
